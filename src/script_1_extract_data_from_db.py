a73b94632044d7f2e13bbb9e8970cf204f6ccce6
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-12-06 00:31:17 +0100

    Add todos, do some refactoring

--- src/script_1_extract_data_from_db.py
+++ src/script_1_extract_data_from_db.py
@@ -9,8 +9,16 @@ import subprocess
 from src import column_functions
 from .helpers.utils import unique, hour_to_timestring
 
-## Set parameter ------------------------
-# basic filter settings
+
+# load config
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+with open(f"{PROJECT_DIR}/config.json") as f:
+    config = json.load(f)
+    # TODO: validate config format
+
+
+# set parameters
+# 1. basic filter settings
 fvsi_thold = 5  # fractional variation in solar intensity < the value - before: 4
 sia_thold = 0.4  # solar intensity average > value
 sza_thold = 75  # solar zenith angle < value
@@ -18,7 +26,7 @@ o2_error = 0.0005
 step_size = 0.1
 flag = 1
 
-# advanced filter settings
+# 2. advanced filter settings
 cluster_interval = np.round(
     1 / 30, 6
 )  # [h], moving window step size -> time distance between resulting measurements
@@ -26,21 +34,27 @@ cluster_window = np.round(1 / 6, 6)  # [h], moving window size
 cluster_start = 4  # Time in UTC, start time for the use of measurements
 cluster_end = 18  # Time in UTC, end time for the use of measurements
 
-# version of dropping the averaged points with low information contend
-version_ofDorp = {
-    "drop": True,
-    "version": 104,  # 'calculation' or Number of maximal measurement points per averaged point (10min interval=104)
-    "percent": 0.2,  # [0,1] only needed when 'version' is equal to 'percent'
-}
 
-# same level as pyproject.toml
-project_dir = "/".join(__file__.split("/")[:-2])
-with open(f"{project_dir}/config.json") as f:
-    config = json.load(f)
-    assert type(config["mysql"]) == dict
-    assert type(config["inversionCSV"]) == dict
+# DROP_CLU_INFO -> version of dropping the averaged points with low information contend
+# drop_clu_info["version"] = 104  # 'calculation' or Number of maximal measurement points per averaged point (10min interval=104)
+# drop_clu_info["percent"] = 0.2  # [0,1] only needed when 'version' is equal to 'percent'
 
 
+def apply_statistical_filters(df, gas, column):
+    return column_functions.filter_DataStat(
+        df,
+        gas=gas,
+        column=column,
+        clu_int=cluster_interval,
+        drop_clu_info={"drop": True, "version": 104, "percent": 0.2},
+        clu_str=cluster_start,
+        clu_end=cluster_end,
+        clu_win=cluster_window,
+        case=["outlier", "rollingMean"],  # , "continuous", "interval"],
+    )
+
+
+# TODO: Move get_commit_sha function to helpers
 commit_sha_process = subprocess.Popen(
     ["git", "rev-parse", "--verify", "HEAD"],
     stdout=subprocess.PIPE,
@@ -51,10 +65,10 @@ COMMIT_SHA = stdout.decode().replace("\n", "").replace(" ", "")
 assert len(COMMIT_SHA) > 0
 
 replacement_dict = {
-    "AUTHOR_NAMES": config["inversionCSV"]["authorNames"],
-    "CONTACT_EMAILS": config["inversionCSV"]["contactEmails"],
+    "AUTHOR_NAMES": map(lambda a: a["name"], config["meta"]["authors"]),
+    "CONTACT_EMAILS": map(lambda a: a["email"], config["meta"]["authors"]),
     "GENERATION_DATE": str(datetime.datetime.now()) + " UTC",
-    "CODE_REPOSITORY": config["inversionCSV"]["codeRepository"],
+    "CODE_REPOSITORY": config["meta"]["codeRepository"],
     "COMMIT_SHA": COMMIT_SHA,
     "SETTING_fvsi_thold": fvsi_thold,
     "SETTING_sia_thold": sia_thold,
@@ -66,8 +80,10 @@ replacement_dict = {
 
 
 def read_database(date_string, remove_calibration_data=True):
-    db_connection = mysql.connector.connect(**config["mysql"])
+    db_connection = mysql.connector.connect(**config["authentication"]["mysql"])
     read = lambda sql_string: pd.read_sql(sql_string, con=db_connection)
+
+    # TODO: Hardcode calibration station/location combinations
 
     if remove_calibration_data:
         df_all = read(
@@ -97,24 +113,13 @@ def read_database(date_string, remove_calibration_data=True):
 
     db_connection.close()
 
+    # TODO: Return as dict structure
     return df_all, df_calibration, df_location, df_spectrometer
 
 
-# add timestamp
-def Time_Stamp(df):
-    df = df.drop(columns=["month", "year"])
-    df = df.reset_index(drop=True)
-    df["TimeStamp"] = [
-        (
-            datetime.datetime.strptime(str(df.Date[j]), "%Y%m%d")
-            + datetime.timedelta(hours=df.Hour[j])
-        )
-        for j in range(len(df.Hour))
-    ]
-    return df.sort_values(by=["ID_Spectrometer", "TimeStamp"]).reset_index(drop=True)
-
-
 def filter_and_return(date_string, df_calibrated, df_location, case):
+
+    # TODO: split function!
     assert case in ["website-raw", "website-filtered", "inversion-filtered"]
 
     ## Physical Filter ----------------------
@@ -134,6 +139,8 @@ def filter_and_return(date_string, df_calibrated, df_location, case):
     else:
         df_filtered = df_calibrated.set_index(["Date", "ID_Spectrometer"])
 
+    # TODO: break into smaller loops
+    # TODO: use dict as datastructure
     ## Gas seperated code ---------
     for gas in ["xco2", "xch4", "xco"]:
         if gas == "xco":
@@ -170,17 +177,7 @@ def filter_and_return(date_string, df_calibrated, df_location, case):
         if (case in ["website-filtered", "inversion-filtered"]) and (
             not df_filtered_droped.empty
         ):
-            df_filteredPlus = column_functions.filter_DataStat(
-                df_filtered_droped,
-                gas=gas,
-                column=column,
-                clu_int=cluster_interval,
-                drop_clu_info=version_ofDorp,
-                clu_str=cluster_start,
-                clu_end=cluster_end,
-                clu_win=cluster_window,
-                case=["outlier", "rollingMean"],  # , "continuous", "interval"],
-            )
+            df_filteredPlus = apply_statistical_filters(df_filtered_droped, gas, column)
         else:
             df_filteredPlus = df_filtered_droped.drop(columns=["ID_Location"])
 
@@ -293,6 +290,7 @@ def filter_and_return(date_string, df_calibrated, df_location, case):
                 .reset_index()
             )
 
+            # TODO: prettify dropping (only one drop call)
             df_corrected_inversion = df_corrected_inversion.drop(
                 columns=["ID_Location", "Direction"]
             )
@@ -310,7 +308,6 @@ def filter_and_return(date_string, df_calibrated, df_location, case):
                 "column_h2o",
                 "column_air",
                 "xair",
-                "column_h2o",
                 "month",
                 "year",
                 "Date",
@@ -324,6 +321,7 @@ def filter_and_return(date_string, df_calibrated, df_location, case):
                 ]
             )
 
+            # TODO: Use dict datastructure for different gas-dfs
             if gas == "xco2":
                 inversion_xco2 = df_corrected_inversion
             elif gas == "xch4":
@@ -366,10 +364,12 @@ def filter_and_return(date_string, df_calibrated, df_location, case):
                     right_on="Hour",
                 )
 
+        # TODO: Move filename for out-file into variable
+        # TODO: Extract svg save into helper function
         merged_df.fillna("NaN").reset_index().rename(
             columns={"Hour": "year_day_hour"}
         ).set_index(["year_day_hour"]).to_csv(
-            f"{project_dir}/data/csv-out-for-inversion/HAM{date_string}.csv"
+            f"{PROJECT_DIR}/data/csv-out-for-inversion/HAM{date_string}.csv"
         )
 
         def replace_from_dict(text):
@@ -377,16 +377,16 @@ def filter_and_return(date_string, df_calibrated, df_location, case):
                 text = text.replace(f"%{key}%", str(replacement_dict[key]))
             return text
 
-        with open(f"{project_dir}/data/inversion-header-template.csv", "r") as f:
+        with open(f"{PROJECT_DIR}/data/inversion-header-template.csv", "r") as f:
             file_lines = list(map(replace_from_dict, f.readlines()))
 
         with open(
-            f"{project_dir}/data/csv-out-for-inversion/HAM{date_string}.csv", "r"
+            f"{PROJECT_DIR}/data/csv-out-for-inversion/HAM{date_string}.csv", "r"
         ) as f:
             file_lines += f.readlines()
 
         with open(
-            f"{project_dir}/data/csv-out-for-inversion/HAM{date_string}.csv", "w"
+            f"{PROJECT_DIR}/data/csv-out-for-inversion/HAM{date_string}.csv", "w"
         ) as f:
             for l in file_lines:
                 f.write(l)
@@ -438,27 +438,27 @@ def run(date_string):
                         cal = get_cal(station)[f"{gas}_calibrationFactor"]
                     except:
                         cal = "NaN"
-                        count += 1
                     replacement_dict.update({f"CALIBRATION_{station}_{gas}": cal})
             if case == "inversion-filtered":
                 filter_and_return(date_string, df_calibrated, df_location, case)
             else:
+                # TODO: extract return into dict datastructure
                 xco, xco2, xch4 = filter_and_return(
                     date_string, df_calibrated, df_location, case
                 )
                 if case == "website-filtered":
                     xco2.round(5).to_csv(
-                        f"{project_dir}/data/csv-in/{date_string}_co2.csv"
+                        f"{PROJECT_DIR}/data/csv-in/{date_string}_co2.csv"
                     )
                     xch4.round(5).to_csv(
-                        f"{project_dir}/data/csv-in/{date_string}_ch4.csv"
+                        f"{PROJECT_DIR}/data/csv-in/{date_string}_ch4.csv"
                     )
                 if case == "website-raw":
                     xco2.round(5).to_csv(
-                        f"{project_dir}/data/csv-in/{date_string}_co2_raw.csv"
+                        f"{PROJECT_DIR}/data/csv-in/{date_string}_co2_raw.csv"
                     )
                     xch4.round(5).to_csv(
-                        f"{project_dir}/data/csv-in/{date_string}_ch4_raw.csv"
+                        f"{PROJECT_DIR}/data/csv-in/{date_string}_ch4_raw.csv"
                     )
                 data_exists = True
 
