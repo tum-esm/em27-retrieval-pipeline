4ebd8c95f6f593cb2e9ad1e9f49a3c2bd943b33e
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-12-06 01:50:28 +0100

    Refactor file names, add save_to_files placeholder script

---
+++ src/script_save_to_files.py
@@ -0,0 +1,11 @@
+def save_to_csv():
+    # TODO
+    pass
+
+
+def save_to_json():
+    # TODO: Create tmp directory
+    # TODO: Save raw and filtered CSVs
+    # TODO: Convert CSVs to JSON
+    # TODO: Remove tmp directory
+    pass
