3e2e2a551ce22bdb4ec16e5d3ad98d0261d0b3c6
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2021-12-20 00:56:30 +0100

    Implement automatic processing
    Close #2

--- run_automatic.py
+++ run_automatic.py
@@ -1,9 +1,11 @@
 import json
 import os
+import datetime
 from src import (
     update_automation_report,
     validate_configuration,
 )
+from run import run_pipeline
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 with open(f"{PROJECT_DIR}/config.json") as f:
@@ -11,6 +13,23 @@ with open(f"{PROJECT_DIR}/config.json") as f:
     validate_configuration.run()
 
 
+def log(message: str):
+    with open(f"{PROJECT_DIR}/automation.log", "a") as f:
+        f.write(str(datetime.datetime.now()) + " UTC - " + message + "\n")
+
+
 if __name__ == "__main__":
     dates = update_automation_report.run()
-    print(dates)
+    if len(dates) > 0:
+        log("-" * 40)
+        log("Starting automatic data extraction")
+        log("-" * 40)
+
+        print(f"Updating {len(dates.keys())} day(s): {list(dates.keys())}")
+        for date in dates.keys():
+            log(f"{date}: {dates[date]}")
+        run_pipeline(dates.keys())
+    else:
+        log("-" * 40)
+        log("Automation found no new data to be processed")
+        log("-" * 40)
