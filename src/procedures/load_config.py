d60f0e89e5334c7af37edfae3e1ebfa8a2401633
Moritz Makowski <moritz.makowski@tum.de>
2022-04-19 17:33:34 +0000

    Add parameter to config.json

--- src/procedures/load_config.py
+++ src/procedures/load_config.py
@@ -8,6 +8,7 @@ PROJECT_DIR = dir(dir(dir(os.path.abspath(__file__))))
 validator = Validator(
     {
         "download_map_data_email": {"type": "string"},
+        "sensors_to_consider": {"type": "list", "schema": {"type": "string"}},
         "sensor_coordinates": {
             "type": "dict",
             "keysrules": {"type": "string", "regex": "[a-z]+"},
@@ -40,5 +41,12 @@ def run():
         raise AssertionError("config.json is not in a valid JSON format")
 
     assert validator.validate(CONFIG), f"Invalid config.json: {validator.errors}"
+    for sensor in CONFIG["sensors_to_consider"]:
+        assert (
+            sensor in CONFIG["sensor_coordinates"].keys()
+        ), f'Invalid config.json: sensor {sensor} not in "sensor_coordinates"'
+        assert (
+            sensor in CONFIG["sensor_serial_numbers"].keys()
+        ), f'Invalid config.json: sensor {sensor} not in "sensor_serial_numbers"'
 
     return CONFIG
