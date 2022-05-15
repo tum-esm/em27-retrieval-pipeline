9bb38ee3e5cb2f689ee6a5cb1c0b6b160bb157eb
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-05-15 21:07:41 +0200

    Improve code quality of dataframe processing (7)
    Simplify calibration

--- src/dataframe_processing/calibration.py
+++ src/dataframe_processing/calibration.py
@@ -1,7 +1,10 @@
+import pandas as pd
 import numpy as np
 
+from src import utils
 
-def apply_calibration(df_data_org, df_cali_org):
+
+def apply_calibration(df_data: pd.DataFrame, df_calibration_factors: pd.DataFrame):
     """
     Function to calibrate measurement data
     :param df_data_org: Pandas DataFrame, containing all data,
@@ -14,59 +17,37 @@ def apply_calibration(df_data_org, df_cali_org):
     last modified: 20.10.2020
     """
 
-    df_cali = df_cali_org.copy()
-    # get full siteID
-    df_cali["ID"] = df_cali["ID_SpectrometerCalibration"].apply(lambda x: x[-2:])
-    np_siteID = df_data_org["ID_Spectrometer"].unique()
-    for site in np_siteID:
-        df_cali.loc[df_cali["ID"] == site[:2], "ID"] = site
+    assert utils.functions.is_subset_of(
+        ["ID_Spectrometer", "Date", "xch4_ppm", "xco2_ppm", "xco_ppb"],
+        df_data.columns,
+    )
+    assert utils.functions.is_subset_of(
+        ["ID_SpectrometerCalibration", "EndDate", "StartDate"],
+        df_calibration_factors.columns,
+    )
+
+    df_calibration_factors["ID"] = df_calibration_factors[
+        "ID_SpectrometerCalibration"
+    ].apply(lambda x: x[-2:])
+    for site in df_data["ID_Spectrometer"].unique():
+        df_calibration_factors.loc[
+            df_calibration_factors["ID"] == site[:2], "ID"
+        ] = site
+
     # set all unknown enddates to inf
-    df_cali.loc[df_cali["EndDate"].isnull(), "EndDate"] = np.inf
+    df_calibration_factors.loc[
+        df_calibration_factors["EndDate"].isnull(), "EndDate"
+    ] = np.inf
 
     # calibrate data
-    df_data = df_data_org.copy()
-    for index, row in df_cali.iterrows():
-        df_data.loc[
-            (df_data["ID_Spectrometer"] == row.ID)
-            & (df_data["Date"] >= row.StartDate)
-            & (df_data["Date"] < row.EndDate),
-            "xch4_ppm",
-        ] = (
-            df_data.loc[
-                (df_data["ID_Spectrometer"] == row.ID)
-                & (df_data["Date"] >= row.StartDate)
-                & (df_data["Date"] < row.EndDate),
-                "xch4_ppm",
-            ]
-            / row.ch4_calibrationFactor
+    for _, row in df_calibration_factors.iterrows():
+        _row_query = (
+            (df_data["ID_Spectrometer"] == row["ID"])
+            & (df_data["Date"] >= row["StartDate"])
+            & (df_data["Date"] < row["EndDate"])
         )
-        df_data.loc[
-            (df_data["ID_Spectrometer"] == row.ID)
-            & (df_data["Date"] >= row.StartDate)
-            & (df_data["Date"] < row.EndDate),
-            "xco2_ppm",
-        ] = (
-            df_data.loc[
-                (df_data["ID_Spectrometer"] == row.ID)
-                & (df_data["Date"] >= row.StartDate)
-                & (df_data["Date"] < row.EndDate),
-                "xco2_ppm",
-            ]
-            / row.co2_calibrationFactor
-        )
-        df_data.loc[
-            (df_data["ID_Spectrometer"] == row.ID)
-            & (df_data["Date"] >= row.StartDate)
-            & (df_data["Date"] < row.EndDate),
-            "xco_ppb",
-        ] = (
-            df_data.loc[
-                (df_data["ID_Spectrometer"] == row.ID)
-                & (df_data["Date"] >= row.StartDate)
-                & (df_data["Date"] < row.EndDate),
-                "xco_ppb",
-            ]
-            / row.co_calibrationFactor
-        )
+        for gas in ["co", "co2", "ch4"]:
+            column_name = f"x{gas}_{utils.constants.UNITS[gas]}"
+            df_data.loc[_row_query, column_name] /= row[f"{gas}_calibrationFactor"]
 
-    return df_data, df_cali
+    return df_data, df_calibration_factors
