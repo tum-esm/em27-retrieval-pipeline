779cbd2f6db3ae9c95e7f871ce17bcb14df23da5
Moritz Makowski <moritz.makowski@tum.de>
2022-05-27 03:43:37 +0000

    Print out queries.json

--- generate_dataset_main.py
+++ generate_dataset_main.py
@@ -6,9 +6,9 @@ from src import generate_dataset as gd
 
 query_list = gd.generate_query_list.run()
 
-print(json.dumps(query_list, indent=4))
-print(len(query_list))
+with open("queries.json", "w") as f:
+    json.dump(query_list, f, indent=4)
 
+print(f"{len(query_list)} queries")
 for query in track(query_list):
-    print(query)
     gd.process_query.run(query)
