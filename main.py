a6de6e08a5e1bb8748a7be83fde99eeea1ebac9c
Moritz Makowski <moritz.makowski@tum.de>
2022-05-28 16:51:44 +0000

    Block if script is already running

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import filelock
+from src.utils import load_setup
 from src import main
 
+PROJECT_DIR, CONFIG = load_setup()
+lock_path = f"{PROJECT_DIR}/main.lock"
+lock = filelock.FileLock(lock_path, timeout=0)
+
+
 if __name__ == "__main__":
-    # TODO: Only run main function if this script is not already running
-    # This can be done by looking for "python .../automated-proffast
-    # -pylot/run.py" inside the stdout from calling "ps -af"
-    main.run()
+    try:
+        lock.acquire()
+        try:
+            main.run()
+        finally:
+            lock.release()
+    except filelock.Timeout:
+        # If you know what you are doing, you can remove the "main.lock" file
+        print("process is already running")
