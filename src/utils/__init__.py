9b0a2ddb3f18f687f3f2ddb253b67fe1adf3c39d
Moritz Makowski <moritz.makowski@tum.de>
2022-06-10 13:04:13 +0000

    Make all procedures consider all src directories

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,3 +2,8 @@ from .load_setup import load_setup
 from .logger import Logger
 from .location_data import LocationData
 from .retrieval_queue import RetrievalQueue
+from .directory_utils import (
+    directories_are_equal,
+    get_existing_src_directories,
+    assert_directory_list_equality,
+)
