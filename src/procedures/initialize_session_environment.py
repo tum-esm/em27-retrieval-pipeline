f88b48ac508d853d7878107cc3efe7ec013ab881
Moritz Makowski <moritz.makowski@tum.de>
2022-06-11 12:01:45 +0000

    Detect proffast error type and write it to logs

--- src/procedures/initialize_session_environment.py
+++ src/procedures/initialize_session_environment.py
@@ -8,11 +8,11 @@ YAML_TEMPLATE = f"{PROJECT_DIR}/src/pylot_config_template.yml"
 
 
 def run(session):
-    session_sensor = session["sensor"]
-    session_lat = session["lat"]
-    session_lon = session["lon"]
-    session_alt = session["alt"]
-    session_serial_number = session["serial_number"]
+    sensor = session["sensor"]
+    lat = session["lat"]
+    lon = session["lon"]
+    alt = session["alt"]
+    serial_number = session["serial_number"]
 
     assert os.path.isfile(
         f"{PROJECT_DIR}/.venv/bin/python"
@@ -42,30 +42,30 @@ def run(session):
         shutil.rmtree(f"{PROJECT_DIR}/{_d}")
         os.mkdir(f"{PROJECT_DIR}/{_d}")
         os.system(f"touch {PROJECT_DIR}/{_d}/.gitkeep")
-    os.mkdir(f"{PROJECT_DIR}/inputs/{session_sensor}_ifg")
-    os.mkdir(f"{PROJECT_DIR}/inputs/{session_sensor}_map")
-    os.mkdir(f"{PROJECT_DIR}/inputs/{session_sensor}_pressure")
+    os.mkdir(f"{PROJECT_DIR}/inputs/{sensor}_ifg")
+    os.mkdir(f"{PROJECT_DIR}/inputs/{sensor}_map")
+    os.mkdir(f"{PROJECT_DIR}/inputs/{sensor}_pressure")
 
     # Create YAML file for proffast
     with open(YAML_TEMPLATE, "r") as _f:
         file_content = "".join(_f.readlines())
 
     replacements = {
-        "SERIAL_NUMBER": str(session_serial_number).zfill(3),
-        "SENSOR": session_sensor,
+        "SERIAL_NUMBER": str(serial_number).zfill(3),
+        "SENSOR": sensor,
         "PROJECT_DIR": PROJECT_DIR,
         "COMMIT_SHA": subprocess.check_output(
             ["git", "rev-parse", "--short", "--verify", "HEAD"], cwd=PROJECT_DIR
         )
         .decode()
         .replace("\n", ""),
-        "COORDINATES_LAT": str(round(session_lat, 3)),
-        "COORDINATES_LON": str(round(session_lon, 3)),
-        "COORDINATES_ALT": str(round(session_alt / 1000.0, 3)),
+        "COORDINATES_LAT": str(round(lat, 3)),
+        "COORDINATES_LON": str(round(lon, 3)),
+        "COORDINATES_ALT": str(round(alt / 1000.0, 3)),
     }
 
     for key, value in replacements.items():
         file_content = file_content.replace(f"%{key}%", value)
 
-    with open(f"{PROJECT_DIR}/inputs/{session_sensor}-pylot-config.yml", "w") as _f:
+    with open(f"{PROJECT_DIR}/inputs/{sensor}-pylot-config.yml", "w") as _f:
         _f.write(file_content)
