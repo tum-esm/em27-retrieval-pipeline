bffb0fbc5bf40c7bb8a5b2c5964d9c75a9484fde
Moritz Makowski <moritz.makowski@tum.de>
2022-06-23 02:39:51 +0000

    Add scripts to fill queue (3)

--- scripts/add-days-without-outputs-to-queue.py
+++ scripts/add-days-without-outputs-to-queue.py
@@ -1,37 +1,13 @@
 import os
-import sys
-import json
 from tqdm import tqdm
-from datetime import datetime
-
-
-dir = os.path.dirname
-PROJECT_DIR = dir(dir(os.path.abspath(__file__)))
-MANUAL_QUEUE_PATH = f"{PROJECT_DIR}/config/manual-queue.json"
-sys.path.append(PROJECT_DIR)
-from src import utils
-
-CONFIG = utils.load_config(validate=True)
-SENSORS = CONFIG["sensorsToConsider"]
-START_DATE = CONFIG["startDate"]
-DST = CONFIG["dst"]
-OUTPUT_DIR = {
-    "2.0.1": "proffast-2.0-outputs",
-    "2.1.1": "proffast-2.1-outputs",
-}[CONFIG["proffastVersion"]]
-PRIORITY = -5
-
-
-def date_string_is_valid(date_string):
-    try:
-        datetime.strptime(str(date_string), "%Y%m%d")
-        return True
-    except:
-        return False
-
-
-with open(MANUAL_QUEUE_PATH, "r") as f:
-    queue: list = json.load(f)
+from utils import (
+    date_string_is_valid,
+    process_queue_additions,
+    START_DATE,
+    SENSORS,
+    PIPELINE_DST,
+    OUTPUT_DIR,
+)
 
 
 queue_additions = []
@@ -42,26 +18,11 @@ for d in tqdm(range(int(START_DATE), 20300000)):
     for s in SENSORS:
         try:
             # conditions: ifgs exist, no outputs exist
-            assert os.path.isdir(f"{DST}/{s}/ifgs/{d}")
-            assert not os.path.isdir(f"{DST}/{s}/{OUTPUT_DIR}/successful/{d}")
-            assert not os.path.isdir(f"{DST}/{s}/{OUTPUT_DIR}/failed/{d}")
+            assert os.path.isdir(f"{PIPELINE_DST}/{s}/ifgs/{d}")
+            assert not os.path.isdir(f"{PIPELINE_DST}/{s}/{OUTPUT_DIR}/successful/{d}")
+            assert not os.path.isdir(f"{PIPELINE_DST}/{s}/{OUTPUT_DIR}/failed/{d}")
             queue_additions.append((s, d))
         except AssertionError:
             pass
 
-for s, d in queue_additions:
-    queue = list(
-        filter(
-            lambda x: not ((x["sensor"] == s) and (x["date"] == str(d))),
-            queue,
-        )
-    )
-    queue.append({"sensor": s, "date": str(d), "priority": PRIORITY})
-
-print(
-    f"Added {len(queue_additions)} timeseries to queue:\n"
-    + " ".join([f"{s}/{d}" for s, d in queue_additions])
-)
-
-with open(MANUAL_QUEUE_PATH, "w") as f:
-    json.dump(queue, f, indent=4)
+process_queue_additions(queue_additions)
---
+++ scripts/utils.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import json
+from datetime import datetime
+
+
+dir = os.path.dirname
+PROJECT_DIR = dir(dir(os.path.abspath(__file__)))
+MANUAL_QUEUE_PATH = f"{PROJECT_DIR}/config/manual-queue.json"
+sys.path.append(PROJECT_DIR)
+from src import utils
+
+CONFIG = utils.load_config(validate=True)
+SENSORS = CONFIG["sensorsToConsider"]
+START_DATE = CONFIG["startDate"]
+PIPELINE_DST = CONFIG["dst"]
+PROFFAST_VERSION = CONFIG["proffastVersion"]
+OUTPUT_DIR = {
+    "2.0.1": "proffast-2.0-outputs",
+    "2.1.1": "proffast-2.1-outputs",
+}[PROFFAST_VERSION]
+PRIORITY = -5
+
+
+def _load_queue() -> list:
+    with open(MANUAL_QUEUE_PATH, "r") as f:
+        return json.load(f)
+
+
+def _save_queue(queue: list):
+    with open(MANUAL_QUEUE_PATH, "w") as f:
+        json.dump(queue, f, indent=4)
+
+
+def _add_to_queue(queue, sensor, date):
+    queue = list(
+        filter(
+            lambda x: not ((x["sensor"] == sensor) and (x["date"] == str(date))),
+            queue,
+        )
+    )
+    queue.append({"sensor": sensor, "date": str(date), "priority": PRIORITY})
+    return queue
+
+
+def date_string_is_valid(date_string):
+    try:
+        datetime.strptime(str(date_string), "%Y%m%d")
+        return True
+    except:
+        return False
+
+
+def process_queue_additions(queue_additions: list):
+    queue = _load_queue()
+    for s, d in queue_additions:
+        queue = _add_to_queue(queue, s, d)
+    _save_queue(queue)
+    print(
+        f"proffastVersion = {PROFFAST_VERSION}, startDate = {START_DATE}\n"
+        f"Added {len(queue_additions)} timeseries to queue:\n"
+        + " ".join([f"{s}/{d}" for s, d in queue_additions])
+    )
