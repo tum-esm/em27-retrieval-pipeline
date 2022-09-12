a8626491e4845c83b007efa5e07867d8fa37c434
Moritz Makowski <moritz@dostuffthatmatters.dev>
2022-09-12 19:27:55 +0000

    #8 #9 - Proffast 2.2 refactoring (11)

--- tests/test_default_config.py
+++ tests/test_default_config.py
@@ -1,6 +1,8 @@
+import pytest
 from src import utils
 from tests.fixtures import provide_tmp_config
 
 
+@pytest.mark.ci
 def test_default_config(provide_tmp_config):
     utils.load_config(validate=True, skip_directory_paths=True)
--- tests/test_pylot.py
+++ tests/test_pylot.py
@@ -1,8 +1,9 @@
 import shutil
+import pytest
 import glob
 import os
 import sys
-from tests.pylot_test_utils import wrap_test_with_mainlock, PROJECT_DIR
+from tests.fixtures import wrap_test_with_mainlock, PROJECT_DIR
 
 sys.path.append(os.path.join(PROJECT_DIR, "src", "prfpylot"))
 from src.prfpylot.prfpylot.pylot import Pylot
@@ -40,25 +41,35 @@ def _write_pylot_config():
 
 def _assert_output_file_integrity():
     output_dir = f"{PROJECT_DIR}/example/pylot_outputs/mc_SN115_220602-220602"
-    for output_file in [
+    expected_output_files = [
         f"pylot_config.yml",
         "input_files/preprocess4mc_220602.inp",
         "input_files/pcxs20mc_220602.inp",
         "input_files/invers20mc_220602_a.inp",
-        "logfiles/pylot_*.log",
         "logfiles/preprocess_output.log",
         "logfiles/pcxs_output.log",
         "logfiles/inv_output.log",
-        "combined_invparms_mc_220602-220602.csv",
-    ]:
-        f = f"{output_dir}/{output_file}"
-        assert len(glob.glob(f)) == 1, f"{f} does not exist"
+        "logfiles/pylot_*.log",
+        "comb_invparms_mc_SN115_220602-220602.csv",
+    ]
+    missing_output_files = list(
+        filter(
+            lambda f: len(glob.glob(f"{output_dir}/{f}")) == 1,
+            expected_output_files,
+        )
+    )
 
-    with open(f"{output_dir}/combined_invparms_mc_220602-220602.csv", "r") as f:
+    assert len(missing_output_files) == 0, (
+        f"Missing {len(missing_output_files)}/{len(expected_output_files)}"
+        + f" output files: {missing_output_files}"
+    )
+
+    with open(f"{output_dir}/comb_invparms_mc_SN115_220602-220602.csv", "r") as f:
         result_line_count = len(f.readlines())
-        assert result_line_count > 2
+        assert result_line_count > 2, "Output comb_invparms_*.csv is empty"
 
 
+@pytest.mark.integration
 def test_pylot(wrap_test_with_mainlock):
     _clean_output_directory()
     pylot_config_file = _write_pylot_config()
