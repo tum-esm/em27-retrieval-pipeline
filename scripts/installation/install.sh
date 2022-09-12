7b659f2b863930d1637756eb14d95031a210931c
Moritz Makowski <moritz@dostuffthatmatters.dev>
2022-09-12 17:14:26 +0000

    #8 #9 - Proffast 2.2 refactoring (4)

--- scripts/installation/install.sh
+++ scripts/installation/install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-init submodule proffaspylot
+# init submodule proffaspylot
 git submodule init
 
 # download and unzip proffast v2.0.1
-wget https://www.imk-asf.kit.edu/downloads/Coccon-SW/PROFFASTv2.0.1.zip
-unzip PROFFASTv2.0.1.zip
-rm PROFFASTv2.0.1.zip
+wget https://www.imk-asf.kit.edu/downloads/Coccon-SW/PROFFASTv2.2.zip
+unzip PROFFASTv2.2.zip
+rm PROFFASTv2.2.zip
 
 # move proffast into correct subdirectory
-rm -r src/pylot_1_0/prf
-mv PROFFASTv2.0.1 src/pylot_1_0/prf
+if [[ -d src/prfpylot/prf ]]
+then
+    rm -r src/prfpylot/prf
+fi
+mv prf src/prfpylot
 
 # compile fortran code
-cd src/pylot_1_0/prf
-bash ../../../scripts/installation/compile-proffast.sh
+cd src/prfpylot/prf
+bash install_proffast_linux.sh 
 cd ../../..
 
 # compile fortran code
