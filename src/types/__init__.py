4661aafb2b3fcb0062daf217eb80526ccebdff78
Moritz Makowski <moritz@dostuffthatmatters.dev>
2022-09-12 21:55:01 +0000

    #8 #9 - Proffast 2.2 refactoring (16)

--- src/types/__init__.py
+++ src/types/__init__.py
@@ -1,2 +1,3 @@
 from .config import ConfigDict, validate_config_dict
+from .manual_queue import ManualQueueItemDict, validate_manual_queue
 from .session import SessionDict
--- src/utils/retrieval_queue.py
+++ src/utils/retrieval_queue.py
@@ -1,48 +1,35 @@
 from datetime import datetime
 import json
 import os
-import cerberus
-
-from requests import JSONDecodeError
-from src.utils import LocationData, Logger
+from typing import Iterator, Optional, TypedDict
+from src import utils, types
 
 dir = os.path.dirname
 PROJECT_DIR = dir(dir(dir(os.path.abspath(__file__))))
 MANUAL_QUEUE_FILE = f"{PROJECT_DIR}/config/manual-queue.json"
 
-manual_queue_validator = cerberus.Validator(
-    {
-        "queue": {
-            "type": "list",
-            "schema": {
-                "type": "dict",
-                "schema": {
-                    "sensor": {"type": "string"},
-                    "date": {"type": "string"},
-                    "priority": {"type": "integer", "forbidden": [0]},
-                },
-            },
-        }
-    }
-)
+
+class SensorDateDict(TypedDict):
+    sensor: str
+    date: str
 
 
-def _date_string_is_valid(
-    date_string: str, start_date: str = None, ignore_profile_delay=False
+def _consider_date_string(
+    date_string: str,
+    start_date: str = None,
+    min_days_delay: int = 1,
 ):
     try:
         now = datetime.now()
-        then = datetime.strptime(date_string, "%Y%m%d")
+        then = datetime.strptime(date_string, "%Y%m%d")  # will have the time 00:00:00
 
-        if not ignore_profile_delay:
-            # vertical profiles are only available with 5 days delay
-            assert (now - then).days >= 5
+        assert (now - then).days >= min_days_delay
 
         if start_date is not None:
-            assert int(date_string) >= start_date
+            assert date_string >= start_date
 
         return True
-    except (AssertionError, ValueError, TypeError):
+    except (AssertionError, ValueError):
         return False
 
 
@@ -51,13 +38,13 @@ class RetrievalQueue:
     Example queue:
     [
         {
-            'sensor': 'mb', 'location': 'FEL', 'date': 20220512,
+            'sensor': 'mb', 'location': 'FEL', 'date': '20220512',
             'lat': 48.148, 'lon': 11.73, 'alt': 536, 'serial_number': 86
         }, {
-            'sensor': 'mb', 'location': 'FEL', 'date': 20220513,
+            'sensor': 'mb', 'location': 'FEL', 'date': '20220513',
             'lat': 48.148, 'lon': 11.73, 'alt': 536, 'serial_number': 86
         }, {
-            'sensor': 'mb', 'location': 'TUM_I', 'date': 20220514,
+            'sensor': 'mb', 'location': 'TUM_I', 'date': '20220514',
             'lat': 48.151, 'lon': 11.569, 'alt': 539, 'serial_number': 86
         },
         ...
@@ -68,56 +55,57 @@ class RetrievalQueue:
     3. Takes all items from manual-queue.json with a priority < 0
     """
 
-    def __init__(self, config: dict):
+    def __init__(self, config: types.ConfigDict):
         self.config = config
-        self.processed_sensor_dates = {}
-        self.location_data = LocationData()
+        self.processed_sensor_dates: dict[str, list[str]] = {}
+        self.location_data = utils.LocationData()
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[types.SessionDict]:
         iteration_count = 0
         while True:
             iteration_count += 1
-            Logger.line()
-            Logger.debug(f"Scheduler: Iteration {iteration_count}")
+            utils.Logger.line()
+            utils.Logger.debug(f"Scheduler: Iteration {iteration_count}")
 
             next_high_prio_queue_item = self._next_item_from_manual_queue(priority=True)
             if next_high_prio_queue_item is not None:
-                Logger.info(
+                utils.Logger.info(
                     "Scheduler: Taking next item from manual queue (high priority)"
                 )
                 yield next_high_prio_queue_item
                 continue
             else:
-                Logger.debug("Scheduler: High priority queue is empty")
+                utils.Logger.debug("Scheduler: High priority queue is empty")
 
             if self.config["processUploadsAutomatically"]:
                 next_upload_directory_item = self._next_item_from_upload_directory()
                 if next_upload_directory_item is not None:
-                    Logger.info("Scheduler: Taking next item from upload directory")
+                    utils.Logger.info(
+                        "Scheduler: Taking next item from upload directory"
+                    )
                     yield next_upload_directory_item
                     continue
                 else:
-                    Logger.debug("Scheduler: Upload directory is empty")
+                    utils.Logger.debug("Scheduler: Upload directory is empty")
             else:
-                Logger.debug(
+                utils.Logger.debug(
                     "Scheduler: Skipping upload queue (processUploadsAutomatically == false)"
                 )
 
             next_low_prio_queue_item = self._next_item_from_manual_queue(priority=False)
             if next_low_prio_queue_item is not None:
-                Logger.info(
+                utils.Logger.info(
                     "Scheduler: Taking next item from manual queue (low priority)"
                 )
                 yield next_low_prio_queue_item
                 continue
             else:
-                Logger.debug("Scheduler: Low priority queue is empty")
+                utils.Logger.debug("Scheduler: Low priority queue is empty")
 
             return
 
     # TODO: Only consider dates from upload dir where upload has been finished
-    #       How to tell, whether an upload has been finished?
-    def _next_item_from_upload_directory(self):
+    def _next_item_from_upload_directory(self) -> Optional[types.SessionDict]:
         """
         Use the dates from /mnt/measurementData/mu
         """
@@ -127,7 +115,9 @@ class RetrievalQueue:
             dates = [
                 x
                 for x in os.listdir(upload_src)
-                if _date_string_is_valid(x[:8], start_date=self.config["startDate"])
+                if _consider_date_string(
+                    x, start_date=self.config["start_date"], min_days_delay=5
+                )
             ]
             sensor_dates += [{"sensor": sensor, "date": date} for date in dates]
 
@@ -146,59 +136,53 @@ class RetrievalQueue:
             next_process = self._generate_process_from_sensor_date(next_sensor_date)
             return next_process
         except:
-            Logger.error(
+            utils.Logger.error(
                 f'Error during "_generate_process_from_sensor_date" '
                 + "for next_sensor_date={next_sensor_date}"
             )
-            Logger.exception()
+            utils.Logger.exception()
             return self._next_item_from_upload_directory()
 
-    def _next_item_from_manual_queue(self, priority: bool = True):
+    def _next_item_from_manual_queue(
+        self, consider_priority_items: bool = True
+    ) -> Optional[types.SessionDict]:
         """
         Use the dates from manual-queue.json
         """
 
-        # TODO: validate manual queue on load
-
         try:
             with open(MANUAL_QUEUE_FILE, "r") as f:
-                sensor_dates = json.load(f)
-            assert manual_queue_validator.validate(
-                {"queue": sensor_dates}
-            ), manual_queue_validator.errors
-
-            for x in sensor_dates:
-                sensor = x["sensor"]
-                date = x["date"]
-                assert (
-                    sensor in self.config["sensorsToConsider"]
-                ), f'sensor "{sensor}" not in config.sensorsToConsider'
-
-                if not _date_string_is_valid(date, ignore_profile_delay=True):
-                    Logger.debug(
-                        f"Scheduler: Skipping {sensor}/{date} "
-                        + "(invalid or too recent date)"
-                    )
-                    self._mark_sensor_date_as_processed(sensor, date)
-        except AssertionError as e:
-            Logger.warning(f"Manual queue in an invalid format: {e}")
-            return None
-        except JSONDecodeError:
-            Logger.warning(f"Manual queue in an invalid JSON format")
-            return None
-        except FileNotFoundError:
+                manual_queue: list[types.ManualQueueItemDict] = json.load(f)
+                types.validate_manual_queue(manual_queue, self.config)
+        except Exception as e:
+            utils.Logger.warning(f"Manual queue in an invalid format: {e}")
             return None
 
-        # priority 0 is not allowed in schema, (0 is the regular queue from upload)
-        sensor_dates = list(
-            filter(
-                lambda x: (x["priority"] > 0) if priority else (x["priority"] < 0),
-                sensor_dates,
-            )
-        )
+        sensor_dates: list[SensorDateDict] = []
 
-        # Do not take in a sensor date multiple times
-        sensor_dates = self._filter_sensor_dates_by_processed_items(sensor_dates)
+        for manual_queue_item in manual_queue:
+            sensor = manual_queue_item["sensor"]
+            date = manual_queue_item["date"]
+            priority = manual_queue_item["priority"]
+
+            if not _consider_date_string(date, min_days_delay=1):
+                utils.Logger.debug(
+                    f"Scheduler: Skipping {sensor}/{date} "
+                    + "(invalid or too recent date)"
+                )
+                self._mark_sensor_date_as_processed(sensor, date)
+
+            if date in self.processed_sensor_dates.get(sensor, []):
+                continue
+
+            if (priority > 0 and (not consider_priority_items)) or (
+                priority < 0 and (consider_priority_items)
+            ):
+                continue
+
+            sensor_dates.append({"sensor": sensor, "date": date})
+
+        # TODO: CONTINUE HERE
 
         if len(sensor_dates) == 0:
             return None
@@ -221,13 +205,15 @@ class RetrievalQueue:
                 }
             )
 
-    def _mark_sensor_date_as_processed(self, sensor: str, date: int):
+    def _mark_sensor_date_as_processed(self, sensor: str, date: str):
         if sensor not in self.processed_sensor_dates.keys():
-            self.processed_sensor_dates[sensor] = [str(date)]
+            self.processed_sensor_dates[sensor] = [date]
         else:
-            self.processed_sensor_dates[sensor].append(str(date))
+            self.processed_sensor_dates[sensor].append(date)
 
-    def _filter_sensor_dates_by_processed_items(self, sensor_dates: list):
+    def _filter_sensor_dates_by_processed_items(
+        self, sensor_dates: list[types.SessionDict]
+    ):
         return list(
             filter(
                 lambda x: x["date"]
@@ -236,7 +222,9 @@ class RetrievalQueue:
             )
         )
 
-    def _generate_process_from_sensor_date(self, sensor_date: dict):
+    def _generate_process_from_sensor_date(
+        self, sensor_date: dict
+    ) -> types.SessionDict:
         sensor = sensor_date["sensor"]
         date = int(sensor_date["date"])
         location = self.location_data.get_location_for_date(sensor, date)
@@ -269,6 +257,6 @@ class RetrievalQueue:
             )
         )
         if len(new_manual_queue_content) < len(old_manual_queue_content):
-            Logger.debug("Removing item from manual queue")
+            utils.Logger.debug("Removing item from manual queue")
             with open(MANUAL_QUEUE_FILE, "w") as f:
                 json.dump(new_manual_queue_content, f, indent=4)
