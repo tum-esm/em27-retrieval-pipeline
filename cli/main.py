a784204f5fdf4d89fdc135a622dbf441e0dab451
Moritz Makowski <moritz@dostuffthatmatters.dev>
2022-10-03 23:26:05 +0000

    #20 - connect commands to cli entrypoint

--- cli/main.py
+++ cli/main.py
@@ -6,10 +6,21 @@ dir = os.path.dirname
 PROJECT_DIR = dir(dir(os.path.abspath(__file__)))
 sys.path.append(PROJECT_DIR)
 
+from cli.commands import (
+    start_pipeline,
+    stop_pipeline,
+    pipeline_is_running,
+)
+
 
 @click.group()
 def cli() -> None:
     pass
+
+
+cli.add_command(start_pipeline, name="start")
+cli.add_command(stop_pipeline, name="stop")
+cli.add_command(pipeline_is_running, name="is-running")
 
 
 if __name__ == "__main__":
