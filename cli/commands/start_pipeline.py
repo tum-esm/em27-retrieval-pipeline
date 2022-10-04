86d2fd7e034212a344d5e52f3e31b13fc516e661
Moritz Makowski <moritz@dostuffthatmatters.dev>
2022-10-04 00:28:35 +0000

    #20 - debug pylot output streams in background process

--- cli/commands/start_pipeline.py
+++ cli/commands/start_pipeline.py
@@ -1,4 +1,4 @@
-import subprocess
+import time
 import click
 import os
 from .. import utils
@@ -6,7 +6,7 @@ from .. import utils
 dir = os.path.dirname
 PROJECT_DIR = dir(dir(dir(os.path.abspath(__file__))))
 INTERPRETER_PATH = os.path.join(PROJECT_DIR, ".venv", "bin", "python")
-CORE_SCRIPT_PATH = os.path.join(PROJECT_DIR, "run-retrieval.py")
+RUN_SCRIPT_PATH = os.path.join(PROJECT_DIR, "run-retrieval.py")
 
 
 @click.command(
@@ -14,13 +14,14 @@ CORE_SCRIPT_PATH = os.path.join(PROJECT_DIR, "run-retrieval.py")
     + "Prevents spawning multiple processes"
 )
 def start_pipeline() -> None:
-    existing_pid = utils.process_is_running()
-    if existing_pid is not None:
-        utils.print_red(f"Background process already exists with PID {existing_pid}")
+    current_pid = utils.process_is_running()
+    if current_pid is not None:
+        utils.print_red(f"Background process already exists with PID {current_pid}")
     else:
-        p = subprocess.Popen(
-            [INTERPRETER_PATH, CORE_SCRIPT_PATH],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        utils.print_green(f"Started background process with PID {p.pid}")
+        os.system(f"nohup {INTERPRETER_PATH} {RUN_SCRIPT_PATH} &")
+        time.sleep(0.5)
+        new_pid = utils.process_is_running()
+        if new_pid is None:
+            utils.print_red(f"Could not start background process")
+        else:
+            utils.print_green(f"Started background process with PID {new_pid}")
