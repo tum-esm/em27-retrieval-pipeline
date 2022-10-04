0aa9a58e3129c52e991763099c1830db010f1c24
Dmytro Polityka <dmytro.polityka@tum.de>
2022-10-04 05:11:19 +0200

    fill table

--- src/config.py
+++ src/config.py
@@ -12,4 +12,4 @@ class Config:
         self.db_password = config.get('db', 'password')
         self.retrieval_version = config.get('db', 'retrieval_version')
         self.database_name = config.get('db', 'database_name')
-        self.csv_locations = config.get('csv', 'csv_locations')
+        self.csv_locations = config.get('csv', 'csv_locations').split(',')
