a0f767512830c74e7217af71158ecef0db07d023
MarlonMueller <marlon.mueller@tum.de>
2022-10-24 15:47:40 +0200

    Catch invalid URIs

--- scripts/compare_commit_hashes.sh
+++ scripts/compare_commit_hashes.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-
-local=$(git -C $LOCAL rev-parse HEAD)
-remote=$(git ls-remote --symref $REMOTE | awk 'NR==2{print $1}')
+set -o pipefail
+{
+    local=$(git -C $LOCAL rev-parse HEAD)
+} || {
+    echo "Local repository not found." >&2
+    return 1
+}
+{
+    remote=$(git ls-remote --symref $REMOTE | awk 'NR==2{print $1}')
+} || {
+    echo "Remote repository not found." >&2
+    return 1
+}
 printf "Local : %s\nRemote: %s\n" $local $remote
-
 if [[ $local == $remote ]]; then
     echo "Commit hashes match."
     return 0
