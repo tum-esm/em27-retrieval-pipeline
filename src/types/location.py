ae09fed162d8341dd6afbbdf710b1d7c33d28625
MarlonMueller <marlon.mueller@tum.de>
2022-10-28 22:32:02 +0200

    Add sensor type

--- src/types/location.py
+++ src/types/location.py
@@ -1,14 +1,9 @@
 from attrs import frozen
 
+
 @frozen
 class Location:
-    """Example
-    details: "..."
-    lat: 48.111
-    lon: 11.547
-    alt: 528
-    """
     details: str
-    lat: float
     lon: float
+    lat: float
     alt: int
