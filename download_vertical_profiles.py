576e8be2660700e1964f9cc29b265725dbfb0626
MarlonMueller <marlon.mueller@tum.de>
2022-10-28 22:33:27 +0200

    Rename main script, remove keyring, parse sensors

---
+++ download_vertical_profiles.py
@@ -0,0 +1,78 @@
+import asyncio
+import os
+import json
+import logging
+
+from pathlib import Path
+from filelock import FileLock
+
+from src import utils
+from src.types import Configuration, Location, Sensor
+
+
+def run() -> None:
+
+    logging.basicConfig(level=logging.INFO)
+
+    try:
+
+        project_path = Path(os.path.abspath(__file__)).parent
+
+        # Load and parse the configuration
+        config_path = os.path.join(project_path, "config", "config.json")
+        with FileLock(config_path + ".lock", timeout=10), open(config_path, "r") as f:
+            config = json.load(f, object_hook=lambda d: Configuration(**d))
+            logging.info(f"Configuration ✅")
+
+        # Async. request locations and sensor locations
+        tasks = ["locations.json", "sensors.json"]
+        urls = [os.path.join(config.location_data, task) for task in tasks]
+        responses = asyncio.run(
+            utils.git_request(urls, config.git_username, config.git_token)
+        )
+
+        # Parse locations and sensor locations
+        for task, response in zip(tasks, responses):
+            response.raise_for_status()
+
+            if task == "locations.json":
+                """Example
+                locations = {
+                    "TUM_G": Location(details="...", lon=11.671, lat=48.261, alt=491),
+                    "TUM_I": Location(details="...", lon=11.569, lat=48.151, alt=539),
+                    ...
+                }
+                """
+                locations: dict[str, Location] = {
+                    location_tag: Location(**data)
+                    for location_tag, data in response.json().items()
+                }
+                logging.info(f"Locations ✅")
+
+            elif task == "sensors.json":
+                """Example
+                sensor_locations = {
+                    "ma": (
+                        Sensor(from_date=date(2019, 1, 1), to_date=date(2019, 31, 12), location="TUM_G",
+                        Sensor(from_date=date(2020, 1, 1), to_date=date(2020, 31, 12), location="TUM_I",
+                        ...
+                    ),
+                    ...
+                }
+                """
+                sensor_locations: dict[str, tuple[Sensor, ...]] = {
+                    sensor_tag: tuple(
+                        Sensor(**location_data) for location_data in data["locations"]
+                    )
+                    for sensor_tag, data in response.json().items()
+                }
+                logging.info(f"Sensors ✅")
+
+
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    run()
--- src/types/configuration.py
+++ src/types/configuration.py
@@ -7,22 +7,11 @@ from datetime import datetime
 from attrs import validators as val
 from attrs import Attribute, frozen, field
 
+from src.utils import str_to_date
+
 TODAY = datetime.utcnow().date()
 PROJECT_DIR = Path(os.path.abspath(__file__)).parents[2]
-DST_DIR = os.path.join(PROJECT_DIR, "location_data")
-
-#NOTE - Utils(?)
-def _date_converter(value: str | date) -> date:
-    match value:
-        case date():
-            return value
-        case str():
-            try:
-                return datetime.strptime(value, "%Y%m%d").date()
-            except ValueError as e:
-                raise ValueError(f"Date format must be %Y%m%d") from e
-        case _:
-            raise TypeError(f"Date must be <class 'str'>")
+DST_DIR = os.path.join(PROJECT_DIR, "vertical-profiles")
 
 @frozen
 class Configuration:
@@ -32,9 +21,12 @@ class Configuration:
     location_data: str = field(
         validator=[val.instance_of(str), val.matches_re(r"(https://.*)|(git@.*)")]
     )
+    
+    git_username: str = field(validator=val.instance_of(str))
+    git_token: str = field(validator=val.instance_of(str))
 
-    from_date: date = field(default=TODAY, converter=_date_converter)
-    to_date: date = field(default=TODAY, converter=_date_converter)
+    from_date: date = field(default=TODAY, converter=str_to_date)
+    to_date: date = field(default=TODAY, converter=str_to_date)
 
     dst_directory: str = field(default=DST_DIR, validator=[val.instance_of(str)])
     max_idle_time: int = field(default=60, validator=[val.instance_of(int), val.gt(0)])
@@ -54,5 +46,5 @@ class Configuration:
     @dst_directory.validator
     def _(self, _:Attribute, value: str) -> None:
         if not os.path.isdir(value):
-            logging.warn(f"Creating empty directory {value}")
+            logging.info(f"Creating empty directory {value}")
             os.makedirs(value)
