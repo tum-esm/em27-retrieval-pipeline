6d42e84cf42cfc9344dcd240651679f330a58baa
MarlonMueller <marlon.mueller@tum.de>
2022-11-02 21:11:39 +0100

    Update network utils

--- src/utils/network.py
+++ src/utils/network.py
@@ -1,17 +1,16 @@
 import httpx
 import asyncio
 
+async def _get_git_url(client: httpx.AsyncClient, url: str, auth: tuple[str, str]) -> httpx.Response:
+    response = await client.get(url=url, auth=auth)
+    response.raise_for_status()
+    return response
 
-async def git_request(
-    urls: list[str], git_username: str, git_token: str
-) -> list[httpx.Response]:
+
+async def get_git_urls(urls: list[str], git_username: str, git_token: str) -> list[httpx.Response]:
     async with httpx.AsyncClient() as client:
-        requests = (
-            client.get(
-                url=url,
-                auth=(git_username, git_token),
-                timeout=10,
-            )
+        tasks = (
+            asyncio.create_task(_get_git_url(client, url, (git_username, git_token)))
             for url in urls
         )
-        return await asyncio.gather(*requests)
+        return await asyncio.gather(*tasks)
