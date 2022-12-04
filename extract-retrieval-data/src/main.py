66b561f57826a2cf177ed0646a1a72df4c1a85f1
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-12-04 03:16:36 +0100

    Add request to config

--- extract-retrieval-data/src/main.py
+++ extract-retrieval-data/src/main.py
@@ -3,5 +3,7 @@ from . import utils, procedures
 
 def run() -> None:
     config = utils.load_config()
-
-    campaign_stations = procedures.get_campaign_stations("sda")
+    campaign_stations = procedures.get_campaign_stations(config.request.campaign_name)
+    campaign_dates = procedures.get_campaign_dates(config.request.campaign_name)
+    print(campaign_stations)
+    print(campaign_dates)
