a350cf065669b0e9cc4fbf3919756154de1ed516
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-12-04 04:24:31 +0100

    Load data into dataframe

--- extract-retrieval-data/src/procedures/read_from_database.py
+++ extract-retrieval-data/src/procedures/read_from_database.py
@@ -1,5 +1,6 @@
 import datetime
 import psycopg
+import pandas as pd
 from typing import Any
 from src import custom_types
 
@@ -38,17 +39,33 @@ def get_raw_station_data(
     results = _run_database_request(
         database_config,
         f"""
-    SELECT
-        utc,
-        gnd_p, gnd_t, app_sza,
-        xh2o, xair, xco2, xch4, xco
-    FROM measurements
-    WHERE
-        retrieval_software = '{proffast_version}' AND
-        sensor = '{station_id}' AND
-        utc >= '{from_date.strftime("%Y-%m-%d")}' AND
-        utc < '{to_date.strftime("%Y-%m-%d")}'
-    """,
+            SELECT
+                utc,
+                gnd_p, gnd_t, app_sza,
+                xh2o, xair, xco2, xch4, xco
+            FROM measurements
+            WHERE
+                retrieval_software = '{proffast_version}' AND
+                sensor = '{station_id}' AND
+                utc >= '{from_date.strftime("%Y-%m-%d")}' AND
+                utc < '{to_date.strftime("%Y-%m-%d")}'
+        """,
     )
 
-    print(results)
+    # (datetime.datetime(2021, 1, 9, 13, 58, 44), Decimal('956.04'), Decimal('269.62'), Decimal('78.47'), 874.756, 0.994154, 417.583, 1.89926, 0.0)
+
+    df = pd.DataFrame(
+        results,
+        columns=[
+            "utc_time",
+            f"{station_id}_gnd_p",
+            f"{station_id}_gnd_t",
+            f"{station_id}_app_sza",
+            f"{station_id}_xh2o",
+            f"{station_id}_xair",
+            f"{station_id}_xco2",
+            f"{station_id}_xch4",
+            f"{station_id}_xco",
+        ],
+    ).set_index("utc_time")
+    print(df)
