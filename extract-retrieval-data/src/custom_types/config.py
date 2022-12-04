4a414c0baf3871e45249798739edc8ca4a369d3c
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-12-04 03:16:36 +0100

    Add request to config

--- extract-retrieval-data/src/custom_types/config.py
+++ extract-retrieval-data/src/custom_types/config.py
@@ -1,5 +1,29 @@
 from pydantic import BaseModel, validator
-from .validators import validate_strict_str
+from .validators import validate_strict_str, validate_date_string, validate_dir_path
+
+
+class RequestConfig(BaseModel):
+    campaign_name: str
+    from_date: str
+    to_date: str
+    dst_dir: str
+
+    _type_validator_string = validator(
+        "*",
+        pre=True,
+        allow_reuse=True,
+    )(validate_strict_str)
+    _type_validator_date_string = validator(
+        "from_date",
+        "to_date",
+        pre=True,
+        allow_reuse=True,
+    )(validate_date_string)
+    _type_validator_dir_path = validator(
+        "dst_dir",
+        pre=True,
+        allow_reuse=True,
+    )(validate_dir_path)
 
 
 class DatabaseConfig(BaseModel):
@@ -10,8 +34,13 @@ class DatabaseConfig(BaseModel):
     database_name: str
     table_name: str
 
-    _type_validator_str = validator("*", pre=True)(validate_strict_str)
+    _type_validator_string = validator(
+        "*",
+        pre=True,
+        allow_reuse=True,
+    )(validate_strict_str)
 
 
 class Config(BaseModel):
     database: DatabaseConfig
+    request: RequestConfig
