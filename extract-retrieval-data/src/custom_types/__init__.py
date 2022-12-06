77eb3d27182b875542bda159da7b5f8ab2e507e6
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-12-06 01:43:53 +0100

    Remove everything from extraction codebase again

--- extract-retrieval-data/src/custom_types/__init__.py
+++
@@ -1,5 +0,0 @@
-from typing import Literal
-from .config import Config, DatabaseConfig
-from .campaign_station import CampaignStation, StationId
-
-Date = str
--- extract-retrieval-data/src/procedures/__init__.py
+++
@@ -1,2 +0,0 @@
-from .get_campaign_data import get_campaign_stations, get_campaign_dates
-from .read_from_database import get_daily_dataframe
