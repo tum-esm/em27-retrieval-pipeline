7eeb437fe979bf105c91ef2540947e78e5fc0f74
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-01-21 17:53:46 +0000

    #55 (4)

--- automated-proffast-pylot/src/procedures/move_ifg_files.py
+++ automated-proffast-pylot/src/procedures/move_ifg_files.py
@@ -1,4 +1,5 @@
 import os
+import re
 import shutil
 import subprocess
 from src import utils, custom_types, detect_corrupt_ifgs
@@ -13,66 +14,44 @@ def run(
     logger: utils.Logger,
     session: custom_types.SessionDict,
 ) -> None:
-    sensor, date = session["sensor"], session["date"]
-    container_id = session["container_id"]
-
-    existing_src_directories = utils.get_existing_src_directories(config, sensor, date)
-
-    # no data for this sensor/date
-    if len(existing_src_directories) == 0:
-        utils.retrieval_queue.RetrievalQueue.remove_from_queue_file(
-            sensor, date, config, logger
-        )
-        raise AssertionError("No ifg directories found")
-
-    if len(existing_src_directories) > 1:
-        utils.assert_directory_equality(existing_src_directories)
-        logger.debug(
-            f"Found multiple ifgs directories (identical): {existing_src_directories}"
-        )
-
-    ifg_src = existing_src_directories[0]
+    # find all filenames of interferograms
+    # possible file name patterns: ma20201123.ifg.0001, ma20220316s0e00a.0001
+    ifg_src_directory = os.path.join(
+        config.data_src.interferograms_dir, session.sensor, session.date
+    )
+    expected_ifg_pattern = re.compile(f"^{session.sensor}\\d{{8}}.*\\.ifg\\.\d+$")
+    filenames = [
+        f
+        for f in os.listdir(ifg_src_directory)
+        if expected_ifg_pattern.match(f) is not None
+    ]
+    logger.debug(f"{len(filenames)} files/directories found in ifg src directory")
 
     # Create empty output directory for that date
-    dst_date_path = f"{DST}/{container_id}/{sensor}_ifg/{date[2:]}"
+    dst_date_path = (
+        f"{DST}/{session.container_id}/{session.sensor}_ifg/{session.date[2:]}"
+    )
     os.mkdir(dst_date_path)
 
-    copied_ifg_count = 0
-
     # move all valid ifg files and rename them properly
-    file_count = len(os.listdir(ifg_src))
-    logger.debug(f"{file_count} files/directories found in ifg src directory")
-    for ifg_file in os.listdir(ifg_src):
-        old_path = f"{ifg_src}/{ifg_file}"
-
-        # two possible filenames:
-        # ma20201123.ifg.0001
-        # ma20220316s0e00a.0001
-        if all(
-            [
-                os.path.isfile(old_path),
-                len(ifg_file.split(".")) >= 2,
-                ifg_file.startswith(f"{sensor}{date}"),
-                ifg_file.split(".")[-1].isnumeric(),
-            ]
-        ):
-            ifg_number = ifg_file.split(".")[-1]
-            shutil.copy(old_path, f"{dst_date_path}/{date[2:]}SN.{ifg_number}")
-            copied_ifg_count += 1
+    for filename in filenames:
+        ifg_number = filename.split(".")[-1]
+        shutil.copy(
+            os.path.join(ifg_src_directory, filename),
+            os.path.join(dst_date_path, f"{session.date[2:]}SN.{ifg_number}"),
+        )
 
     # remove corrupt_ifgs
     try:
-        corrupt_files = list(detect_corrupt_ifgs.main.run(dst_date_path).keys())
+        corrupt_filenames = list(detect_corrupt_ifgs.main.run(dst_date_path).keys())
     except subprocess.CalledProcessError:
         raise AssertionError("corrupt-files-detection has failed during execution")
 
-    if len(corrupt_files) > 0:
-        logger.debug(f"Removing {len(corrupt_files)} corrupt file(s): {corrupt_files}")
-        for f in corrupt_files:
-            os.remove(f"{dst_date_path}/{f}")
+    if len(corrupt_filenames) > 0:
+        logger.debug(
+            f"Removing {len(corrupt_filenames)} corrupt file(s): {corrupt_filenames}"
+        )
+        for filename in corrupt_filenames:
+            os.remove(os.path.join(dst_date_path, filename))
     else:
         logger.debug(f"No corrupt files found")
-
-    assert (
-        copied_ifg_count > 0
-    ), f"no ifgs in src directory, sourcing data from {ifg_src}"
