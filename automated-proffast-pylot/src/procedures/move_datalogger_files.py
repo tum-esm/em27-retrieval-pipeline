d171cafba557c0077a3abc38b4fa5d0ea8fefe63
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-02-13 12:08:28 +0000

    Use pressure calibration factors (3)
    use pylots built in thing

--- automated-proffast-pylot/src/procedures/move_datalogger_files.py
+++ automated-proffast-pylot/src/procedures/move_datalogger_files.py
@@ -1,28 +1,6 @@
 import os
-import polars as pl
+import shutil
 from src import utils, custom_types
-
-
-def load_datalogger_df(path: str) -> pl.DataFrame:
-    df = pl.read_csv(
-        path,
-        has_header=True,
-        columns=[
-            "UTCtime___",
-            "BaroTHB40",
-            "BaroYoung",
-            "GPSAlt",
-            "GPSLat",
-            "GPSLong",
-            "GPSTDiff",
-            "GPShdop",
-            "HygroTHB40",
-            "ThermoTHB40",
-            "UTCdate_____",
-            "UTCsec____",
-        ],
-    )
-    return df
 
 
 def run(
@@ -42,17 +20,15 @@ def run(
     )
 
     assert os.path.isfile(src_filepath), "no datalogger file found"
-    raw_df = load_datalogger_df(src_filepath)
+    with open(src_filepath) as f:
+        log_file_line_count = f.read().count("\n") + 1
 
     # 1440 minutes per day + 1 header line
-    if len(raw_df) < 1441:
+    if log_file_line_count < 1441:
         logger.warning(
-            f"{session.sensor_id}/{session.date} - datalogger file only has {len(raw_df)}/1441 lines"
+            f"{session.sensor_id}/{session.date} - datalogger file only has {log_file_line_count}/1441 lines"
         )
-    assert len(raw_df) >= 120, "datalogger file has less than 120 entries"
+    assert log_file_line_count >= 120, "datalogger file has less than 120 entries"
 
     # apply pressure calibration and save file
-    calibrated_df = raw_df.with_columns(
-        pl.col("BaroYoung") * session.pressure_calibration_factor
-    )
-    calibrated_df.write_csv(dst_filepath, null_value="NaN")
+    shutil.copyfile(src_filepath, dst_filepath)
