c271d56be1186dbcfc490f8e25af677e53c954d4
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-04-18 16:48:58 +0200

    #57 - modify pre processing routine

--- src/procedures/automated_proffast/move_ifg_files.py
+++ src/procedures/automated_proffast/move_ifg_files.py
@@ -1,6 +1,5 @@
 import os
 import re
-import shutil
 import subprocess
 
 import tum_esm_utils
@@ -12,6 +11,12 @@ def run(
     logger: utils.automated_proffast.Logger,
     pylot_session: custom_types.PylotSession,
 ) -> None:
+    """Move interferogram files from the source directory to the input directory.
+
+    Accepted file name pattern: `regex(^$(SENSORID)$(DATE).*\.\d+$)`
+
+    Examples: `ma20201123.ifg.0001`, `ma20220316s0e00a.0001`"""
+
     # find all filenames of interferograms
     # possible file name patterns: ma20201123.ifg.0001, ma20220316s0e00a.0001
     ifg_src_directory = os.path.join(
@@ -19,46 +24,62 @@ def run(
         pylot_session.sensor_id,
         pylot_session.date,
     )
-    expected_ifg_pattern = re.compile(r"^" + pylot_session.sensor_id + r"\d{8}.*\.\d+$")
-    filenames = [
-        f
-        for f in os.listdir(ifg_src_directory)
-        if expected_ifg_pattern.match(f) is not None
-    ]
-    logger.debug(
-        f"{len(filenames)} files/directories found in ifg src directory ({ifg_src_directory})"
-    )
-    assert len(filenames) > 0, "no ifg input files"
 
-    # Create empty output directory for that date
-    dst_date_path = os.path.join(
-        pylot_session.data_input_path, "ifg", pylot_session.date[2:]
+    # TODO: make ifg regex configurable
+    expected_ifg_pattern = re.compile(
+        r"^" + pylot_session.sensor_id + pylot_session.date + r".*\.\d+$"
     )
-    os.mkdir(dst_date_path)
 
-    # move all valid ifg files and rename them properly
-    for filename in filenames:
-        ifg_number = filename.split(".")[-1]
-        shutil.copy(
-            os.path.join(ifg_src_directory, filename),
-            os.path.join(dst_date_path, f"{pylot_session.date[2:]}SN.{ifg_number}"),
+    ifg_filenames = list(
+        sorted(
+            [
+                f
+                for f in os.listdir(ifg_src_directory)
+                if expected_ifg_pattern.match(f) is not None
+            ]
         )
+    )
+    # TODO: log used regex
+    logger.debug(
+        f"{len(ifg_filenames)} files/directories found "
+        + f"in ifg src directory ({ifg_src_directory})"
+    )
+    assert len(ifg_filenames) > 0, "no ifg input files"
 
-    # remove corrupt_ifgs
+    # (optional) make interferogram files read-only
+    if (
+        config.automated_proffast.modified_ifg_file_permissions.during_processing
+        is not None
+    ):
+        for f in ifg_filenames:
+            tum_esm_utils.shell.change_file_permissions(
+                os.path.join(ifg_src_directory, f),
+                config.automated_proffast.modified_ifg_file_permissions.during_processing,
+            )
+
+    # exclude corrupt interferograms from list of interferograms
     try:
         corrupt_filenames = list(
             tum_esm_utils.interferograms.detect_corrupt_ifgs(
-                ifg_directory=dst_date_path
+                ifg_directory=ifg_src_directory
             ).keys()
         )
     except subprocess.CalledProcessError:
         raise AssertionError("corrupt-files-detection has failed during execution")
 
-    if len(corrupt_filenames) > 0:
-        logger.debug(
-            f"Removing {len(corrupt_filenames)} corrupt file(s): {corrupt_filenames}"
+    logger.debug(
+        f"Excluding {len(corrupt_filenames)} corrupt file(s) from retrieval"
+        + (f" ({', '.join(corrupt_filenames)})" if len(corrupt_filenames) > 0 else "")
+    )
+    valid_ifg_filenames = [f for f in ifg_filenames if f not in corrupt_filenames]
+
+    # symlink all valid ifg files and rename them to the format expected by the Pylot
+    dst_date_path = os.path.join(
+        pylot_session.data_input_path, "ifg", pylot_session.date[2:]
+    )
+    os.mkdir(dst_date_path)
+    for ifg_index, filename in enumerate(valid_ifg_filenames):
+        os.symlink(
+            os.path.join(ifg_src_directory, filename),
+            os.path.join(dst_date_path, f"{pylot_session.date[2:]}SN.{ifg_index + 1}"),
         )
-        for filename in corrupt_filenames:
-            os.remove(os.path.join(dst_date_path, filename))
-    else:
-        logger.debug(f"No corrupt files found")
