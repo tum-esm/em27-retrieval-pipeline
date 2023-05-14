bee70c410e362121293943edacb620d90c18db93
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-05-14 16:25:16 +0200

    Convert template to typescript (2)
    code and footer

--- docs/src/components/Footer.tsx
+++ docs/src/components/Footer.tsx
@@ -4,24 +4,31 @@ import { useRouter } from 'next/router'
 import { Button } from '@/components/Button'
 import { navigation } from '@/components/Navigation'
 
-function PageLink({ label, page, previous = false }) {
+function PageLink(props: {
+  label: string
+  page: {
+    title: string
+    href: string
+  }
+  previous?: boolean
+}) {
   return (
     <>
       <Button
-        href={page.href}
-        aria-label={`${label}: ${page.title}`}
+        href={props.page.href}
+        aria-label={`${props.label}: ${props.page.title}`}
         variant="secondary"
-        arrow={previous ? 'left' : 'right'}
+        arrow={props.previous ? 'left' : 'right'}
       >
-        {label}
+        {props.label}
       </Button>
       <Link
-        href={page.href}
+        href={props.page.href}
         tabIndex={-1}
         aria-hidden="true"
         className="text-base font-semibold transition text-zinc-900 hover:text-zinc-600 dark:text-white dark:hover:text-zinc-300"
       >
-        {page.title}
+        {props.page.title}
       </Link>
     </>
   )
@@ -61,7 +68,7 @@ function PageNavigation() {
   )
 }
 
-function WebsiteIcon(props) {
+function WebsiteIcon() {
   return (
     <svg
       width="20"
@@ -73,8 +80,7 @@ function WebsiteIcon(props) {
       strokeLinecap="round"
       strokeLinejoin="round"
       className="w-5 h-5 transition stroke-zinc-600 group-hover:stroke-zinc-800 dark:group-hover:stroke-zinc-400"
-      ariaHidden="true"
-      {...props}
+      aria-hidden="true"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
       <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
@@ -86,7 +92,7 @@ function WebsiteIcon(props) {
   )
 }
 
-function GitHubIcon(props) {
+function GitHubIcon() {
   return (
     <svg
       width="20"
@@ -96,8 +102,7 @@ function GitHubIcon(props) {
       strokeLinecap="round"
       strokeLinejoin="round"
       className="w-5 h-5 transition fill-zinc-600 group-hover:fill-zinc-800 dark:group-hover:fill-zinc-400"
-      ariaHidden="true"
-      {...props}
+      aria-hidden="true"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M5.315 2.1c.791 -.113 1.9 .145 3.333 .966l.272 .161l.16 .1l.397 -.083a13.3 13.3 0 0 1 4.59 -.08l.456 .08l.396 .083l.161 -.1c1.385 -.84 2.487 -1.17 3.322 -1.148l.164 .008l.147 .017l.076 .014l.05 .011l.144 .047a1 1 0 0 1 .53 .514a5.2 5.2 0 0 1 .397 2.91l-.047 .267l-.046 .196l.123 .163c.574 .795 .93 1.728 1.03 2.707l.023 .295l.007 .272c0 3.855 -1.659 5.883 -4.644 6.68l-.245 .061l-.132 .029l.014 .161l.008 .157l.004 .365l-.002 .213l-.003 3.834a1 1 0 0 1 -.883 .993l-.117 .007h-6a1 1 0 0 1 -.993 -.883l-.007 -.117v-.734c-1.818 .26 -3.03 -.424 -4.11 -1.878l-.535 -.766c-.28 -.396 -.455 -.579 -.589 -.644l-.048 -.019a1 1 0 0 1 .564 -1.918c.642 .188 1.074 .568 1.57 1.239l.538 .769c.76 1.079 1.36 1.459 2.609 1.191l.001 -.678l-.018 -.168a5.03 5.03 0 0 1 -.021 -.824l.017 -.185l.019 -.12l-.108 -.024c-2.976 -.71 -4.703 -2.573 -4.875 -6.139l-.01 -.31l-.004 -.292a5.6 5.6 0 0 1 .908 -3.051l.152 -.222l.122 -.163l-.045 -.196a5.2 5.2 0 0 1 .145 -2.642l.1 -.282l.106 -.253a1 1 0 0 1 .529 -.514l.144 -.047l.154 -.03z" />
@@ -105,11 +110,16 @@ function GitHubIcon(props) {
   )
 }
 
-function SocialLink({ href, icon: Icon, children }) {
+function SocialLink(props: {
+  href: string
+  variant: 'website' | 'github'
+  srLabel: string
+}) {
   return (
-    <Link href={href} className="group">
-      <span className="sr-only">{children}</span>
-      <Icon />
+    <Link href={props.href} className="group">
+      <span className="sr-only">{props.srLabel}</span>
+      {props.variant === 'website' && <WebsiteIcon />}
+      {props.variant === 'github' && <GitHubIcon />}
     </Link>
   )
 }
@@ -122,15 +132,16 @@ function SmallPrint() {
         {new Date().getFullYear()}. All rights reserved.
       </p>
       <div className="flex gap-4">
-        <SocialLink href="https://www.ee.cit.tum.de/en/esm" icon={WebsiteIcon}>
-          Visit our research group&#39;s website
-        </SocialLink>
+        <SocialLink
+          href="https://www.ee.cit.tum.de/en/esm"
+          variant="website"
+          srLabel="Visit our research group's website"
+        />
         <SocialLink
           href="https://github.com/tum-esm/automated-retrival-pipeline"
-          icon={GitHubIcon}
-        >
-          Follow us on GitHub
-        </SocialLink>
+          variant="github"
+          srLabel="Follow us on GitHub"
+        />
       </div>
     </div>
   )
