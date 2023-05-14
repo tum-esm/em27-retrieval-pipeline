f71024fdfc7daa82bf60e9767371063d6243b2cc
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-05-14 18:54:51 +0200

    Convert template to typescript (10)
    debug

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -15,10 +15,7 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  experimental: {
-    scrollRestoration: true,
-  },
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'],
 }
 
 export default withMDX(nextConfig)
