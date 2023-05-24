f5f7f08318be6debf0c9afc90516bc3b67e5aab7
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2023-05-25 00:12:12 +0200

    Improve config documentation (3)
    add node and python dependencies

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
             fontFamily: {
                 serif: ['var(--next-font-google-crimson-pro)', 'serif'],
             },
+            fontSize: {
+                '2xs': ['0.625rem', '0.75rem'],
+            },
         },
     },
     plugins: [],
