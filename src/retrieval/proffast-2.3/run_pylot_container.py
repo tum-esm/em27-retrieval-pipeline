f836374e394ec474786a2b2db870ca76a5345990
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-08-22 22:56:10 +0200

    #71 (1)
    add installation script
    add run-container script

---
+++ src/retrieval/proffast-2.3/run_pylot_container.py
@@ -0,0 +1,39 @@
+#!/bin/python
+"""
+Script to run a particular Pylot instance
+
+example:
+
+./run.py container_id config_path
+"""
+
+import sys
+import os
+import importlib
+import tum_esm_utils
+
+_PROJECT_DIR = tum_esm_utils.files.get_parent_dir_path(__file__, current_depth=4)
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 3, (
+        "wrong number of arguments provided to run.py. Example"
+        + ' call: "./run.py container_id pylot_config_path"'
+    )
+
+    container_id, pylot_config_path = sys.argv[1:]
+    container_path = os.path.join(
+        _PROJECT_DIR,
+        "data",
+        "containers",
+        f"pylot-container-{container_id}",
+    )
+    assert os.path.isdir(container_path), "container does not exist"
+
+    print(
+        f'executing in container_id "{container_id}" '
+        + f'at container_path "{container_path}" and '
+        + f'pylot_config_path "{pylot_config_path}".'
+    )
+    sys.path.append(container_path)
+    pylot = importlib.import_module("prfpylot.pylot")
+    pylot.Pylot(pylot_config_path, logginglevel="debug").run(n_processes=1)
