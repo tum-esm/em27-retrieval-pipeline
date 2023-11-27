f4e651049efdecad44802b4ec85c2a86064c58b9
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-11-27 14:29:17 +0000

    Remove old CLI

--- cli.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# found at https://dirask.com/posts/Bash-get-current-script-directory-path-1X9E8D
-
-# the automated-retrieval-pipeline directory
-PROJECT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
-"$PROJECT_DIR"/.venv/bin/python "$PROJECT_DIR"/src/cli.py "$@";
