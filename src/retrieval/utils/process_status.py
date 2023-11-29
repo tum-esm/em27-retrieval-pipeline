5aaff82336c5980ccb61f3f76dd49260352472db
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-11-29 02:38:35 +0000

    #74 Make active queue more visible (4)
    Refine process status interface

--- src/retrieval/utils/process_status.py
+++ src/retrieval/utils/process_status.py
@@ -12,9 +12,9 @@ _PROJECT_DIR = tum_esm_utils.files.get_parent_dir_path(
 
 
 class ProcessStatus(pydantic.BaseModel):
-    container_id: str
+    container_id: Optional[str] = None
     sensor_id: str
-    date: datetime.date
+    from_datetime: datetime.datetime
     location_id: str
     ifg_count: Optional[int] = None
     process_start_time: Optional[datetime.datetime] = None
@@ -54,12 +54,11 @@ class ProcessStatusList(pydantic.RootModel[list[ProcessStatus]]):
                 f.write("[]")
 
     @staticmethod
-    def add_item(
-        container_id: str,
-        sensor_id: str,
-        date: datetime.date,
-        location_id: str,
-    ) -> None:
+    def add_items(items: list[tuple[str, datetime.datetime, str]]) -> None:
+        """Add items to the active process list.
+        
+        Args:
+            items: A list of tuples of the form (sensor_id, date, location_id)."""
         with ProcessStatusList.with_filelock():
             try:
                 with open("data/logs/active-processes.json", "r") as f:
@@ -68,20 +67,22 @@ class ProcessStatusList(pydantic.RootModel[list[ProcessStatus]]):
                     )
             except pydantic.ValidationError:
                 return
-            process_list.root.append(
-                ProcessStatus(
-                    container_id=container_id,
-                    sensor_id=sensor_id,
-                    date=date,
-                    location_id=location_id,
+            for sensor_id, from_datetime, location_id in items:
+                process_list.root.append(
+                    ProcessStatus(
+                        sensor_id=sensor_id,
+                        from_datetime=from_datetime,
+                        location_id=location_id,
+                    )
                 )
-            )
             with open("data/logs/active-processes.json", "w") as f:
                 f.write(process_list.model_dump_json(indent=4))
 
     @staticmethod
     def update_item(
-        container_id: str,
+        sensor_id: str,
+        from_datetime: str,
+        container_id: Optional[str] = None,
         ifg_count: Optional[int] = None,
         process_start_time: Optional[datetime.datetime] = None,
         process_end_time: Optional[datetime.datetime] = None
@@ -92,10 +93,15 @@ class ProcessStatusList(pydantic.RootModel[list[ProcessStatus]]):
         except pydantic.ValidationError:
             return
         for p in process_list.root:
-            if p.container_id == container_id:
-                p.ifg_count = ifg_count
-                p.process_start_time = process_start_time
-                p.process_end_time = process_end_time
+            if p.sensor_id == sensor_id and p.from_datetime == from_datetime:
+                if container_id is not None:
+                    p.container_id = container_id
+                if ifg_count is not None:
+                    p.ifg_count = ifg_count
+                if process_start_time is not None:
+                    p.process_start_time = process_start_time
+                if process_end_time is not None:
+                    p.process_end_time = process_end_time
                 break
         with open("data/logs/active-processes.json", "w") as f:
             f.write(process_list.model_dump_json(indent=4))
