588645b384f79e5eca17f8d9ced87d18a7554fcd
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-01-07 16:42:56 +0000

    Upgrade to `em27-metadata@1.0.0-rc.6` (4)
    Fix more mypy issues

--- tests/profiles/test_list_requested_data.py
+++ tests/profiles/test_list_requested_data.py
@@ -10,57 +10,61 @@ from ..fixtures import provide_config_template
 @pytest.mark.quick
 def test_list_requested_data(provide_config_template: src.types.Config) -> None:
     metadata = em27_metadata.EM27MetadataInterface(
-        locations=[
-            em27_metadata.types.LocationMetadata(
-                location_id="l1", details="l1 details", lat=1, lon=2, alt=0
-            ),
-            em27_metadata.types.LocationMetadata(
-                location_id="l2", details="l2 details", lat=1, lon=3, alt=0
-            ),
-            em27_metadata.types.LocationMetadata(
-                location_id="l3", details="l3 details", lat=2, lon=3, alt=0
-            ),
-        ],
-        sensors=[
-            em27_metadata.types.SensorMetadata(
-                sensor_id="s1",
-                serial_number=1,
-                locations=[
-                    em27_metadata.types.SensorTypes.Location(
-                        from_datetime="2000-01-01T00:00:00+00:00",
-                        to_datetime="2000-03-01T11:59:59+00:00",
-                        location_id="l1",
-                    ),
-                    em27_metadata.types.SensorTypes.Location(
-                        from_datetime="2000-03-01T12:00:00+00:00",
-                        to_datetime="2000-05-01T23:59:59+00:00",
-                        location_id="l3",
-                    ),
-                    em27_metadata.types.SensorTypes.Location(
-                        from_datetime="2000-05-04T12:00:00+00:00",
-                        to_datetime="2000-05-07T23:59:59+00:00",
-                        location_id="l2",
-                    ),
-                ],
-            ),
-            em27_metadata.types.SensorMetadata(
-                sensor_id="s2",
-                serial_number=2,
-                locations=[
-                    em27_metadata.types.SensorTypes.Location(
-                        from_datetime="2000-01-07T00:00:00+00:00",
-                        to_datetime="2000-02-23T23:59:59+00:00",
-                        location_id="l1",
-                    ),
-                    em27_metadata.types.SensorTypes.Location(
-                        from_datetime="2000-05-05T12:00:00+00:00",
-                        to_datetime="2000-05-08T23:59:59+00:00",
-                        location_id="l2",
-                    ),
-                ],
-            ),
-        ],
-        campaigns=[],
+        locations=em27_metadata.types.LocationMetadataList(
+            root=[
+                em27_metadata.types.LocationMetadata(
+                    location_id="l1", details="l1 details", lat=1, lon=2, alt=0
+                ),
+                em27_metadata.types.LocationMetadata(
+                    location_id="l2", details="l2 details", lat=1, lon=3, alt=0
+                ),
+                em27_metadata.types.LocationMetadata(
+                    location_id="l3", details="l3 details", lat=2, lon=3, alt=0
+                ),
+            ]
+        ),
+        sensors=em27_metadata.types.SensorMetadataList(
+            root=[
+                em27_metadata.types.SensorMetadata(
+                    sensor_id="s1",
+                    serial_number=1,
+                    setups=[
+                        em27_metadata.types.SetupsListItem(
+                            from_datetime="2000-01-01T00:00:00+00:00",
+                            to_datetime="2000-03-01T11:59:59+00:00",
+                            value=em27_metadata.types.Setup(location_id="l1"),
+                        ),
+                        em27_metadata.types.SetupsListItem(
+                            from_datetime="2000-03-01T12:00:00+00:00",
+                            to_datetime="2000-05-01T23:59:59+00:00",
+                            value=em27_metadata.types.Setup(location_id="l3"),
+                        ),
+                        em27_metadata.types.SetupsListItem(
+                            from_datetime="2000-05-04T12:00:00+00:00",
+                            to_datetime="2000-05-07T23:59:59+00:00",
+                            value=em27_metadata.types.Setup(location_id="l2"),
+                        ),
+                    ],
+                ),
+                em27_metadata.types.SensorMetadata(
+                    sensor_id="s2",
+                    serial_number=2,
+                    setups=[
+                        em27_metadata.types.SetupsListItem(
+                            from_datetime="2000-01-07T00:00:00+00:00",
+                            to_datetime="2000-02-23T23:59:59+00:00",
+                            value=em27_metadata.types.Setup(location_id="l1"),
+                        ),
+                        em27_metadata.types.SetupsListItem(
+                            from_datetime="2000-05-05T12:00:00+00:00",
+                            to_datetime="2000-05-08T23:59:59+00:00",
+                            value=em27_metadata.types.Setup(location_id="l2"),
+                        ),
+                    ],
+                ),
+            ]
+        ),
+        campaigns=em27_metadata.types.CampaignMetadataList(root=[]),
     )
     expected_data = {
         ProfilesQueryLocation(lat=1, lon=2):
