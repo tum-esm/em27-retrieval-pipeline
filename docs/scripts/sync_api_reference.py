41a4b4fa056dfda612a1fdd08c42a4a436a17146
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-01-20 15:50:03 +0100

    #77 Write Documentation (1)
    Merge export cli and export config scripts

--- docs/scripts/sync_api_reference.py
+++ docs/scripts/sync_api_reference.py
@@ -1,11 +1,62 @@
 import json
+import re
+from typing import Optional
 import os
 import tum_esm_utils
 import sys
 import jsonref
-import re
+import click.core
 
 PROJECT_DIR = tum_esm_utils.files.get_parent_dir_path(__file__, current_depth=3)
+sys.path.append(PROJECT_DIR)
+import src, cli
+
+# ---------------------------------------------------------
+# EXPORT CLI REFERENCE
+
+# credits to https://stackoverflow.com/a/58018765/8255842
+
+
+def recursive_help(
+    command: click.Group | click.Command,
+    parent_context: Optional[click.core.Context] = None,
+) -> str:
+    output: str = ""
+    context = click.core.Context(
+        command, info_name=command.name, parent=parent_context
+    )
+    if isinstance(command, click.Group):
+        for sub_command in command.commands.values():
+            output += recursive_help(sub_command, parent_context=context)
+    else:
+        output += f"## `{context.command_path[4:]}`\n\n"
+        output += command.get_help(context).replace(
+            "\n  ",
+            "\n",
+        ).replace(
+            f"Usage: {context.command_path}",
+            f"**Usage: python cli.py {context.command_path[4:]}",
+        ).replace(
+            "[OPTIONS]",
+            "[OPTIONS]**",
+        ).replace(
+            "Options:\n",
+            "**Options:**\n\n",
+        ) + "\n\n"
+    return output
+
+
+with open(
+    os.path.join(
+        PROJECT_DIR, "docs", "pages", "docs", "api-reference", "cli.mdx"
+    ), "w"
+) as f:
+    f.write("# CLI Reference\n\n")
+    f.write(recursive_help(cli.cli))
+
+# ---------------------------------------------------------
+# EXPORT JSON SCHEMAS
+
 CONFIG_JSON_TARGET = os.path.join(
     PROJECT_DIR, "docs", "components", "config-schema-object.ts"
 )
@@ -18,12 +69,10 @@ MD_FILE_TARGET = os.path.join(
 )
 sys.path.append(PROJECT_DIR)
 
-from src import custom_types
-
 # ---------------------------------------------------------
 # Update the config schema object in the documentation
 
-config_schema_str = custom_types.Config.schema_json(indent=2)
+config_schema_str = json.dumps(src.types.Config.model_json_schema(), indent=2)
 dereferenced_config_schema = jsonref.loads(config_schema_str)
 with open(CONFIG_JSON_TARGET, "w") as f:
     f.write(
@@ -33,19 +82,9 @@ with open(CONFIG_JSON_TARGET, "w") as f:
     )
 
 # ---------------------------------------------------------
-# Update the manual queue schema object in the documentation
-
-manual_queue_schema_str = custom_types.ManualQueue.schema_json(indent=2)
-dereferenced_manual_queue_schema = jsonref.loads(manual_queue_schema_str)
-with open(MANUAL_QUEUE_JSON_TARGET, "w") as f:
-    f.write(
-        "/* prettier-ignore */\n" + "const MANUAL_QUEUE_SCHEMA_OBJECT: any = " +
-        json.dumps(dereferenced_manual_queue_schema, indent=2) +
-        ";\n\nexport default MANUAL_QUEUE_SCHEMA_OBJECT;"
-    )
-
-# ---------------------------------------------------------
 # Update the example files in the documentation
+"""
+TODO: renew this
 
 with open(MD_FILE_TARGET) as f:
     md_file_content = f.read()
@@ -59,13 +98,6 @@ assert len(example_file_blocks[0]) > len(example_file_blocks[1])
 with open(os.path.join(PROJECT_DIR, "config", "config.template.json")) as f:
     config_template_content = f.read()
 
-with open(
-    os.path.join(PROJECT_DIR, "config", "manual-queue.template.json")
-) as f:
-    manual_queue_template_content = f.read()
-
-assert len(config_template_content) > len(manual_queue_template_content)
-
 md_file_content = md_file_content.replace(
     example_file_blocks[0],
     f"Example File\n\n```json\n{config_template_content.strip()}\n```",
@@ -77,3 +109,4 @@ md_file_content = md_file_content.replace(
 
 with open(MD_FILE_TARGET, "w") as f:
     f.write(md_file_content)
+"""
