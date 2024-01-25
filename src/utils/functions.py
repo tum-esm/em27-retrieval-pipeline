d408ad4574d52e131697c7fb99fd65ff5fe19dca
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-01-25 17:03:31 +0100

    #90 Add option to use local pressure in PCSX (1)
    Add function to compute solar noon time

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,6 +1,11 @@
 import datetime
 import tomllib
+from typing import Any
 import em27_metadata
+import skyfield.iokit
+import skyfield.api
+import skyfield.timelib
+import skyfield.almanac
 import tum_esm_utils
 
 
@@ -30,3 +35,35 @@ def get_pipeline_version() -> str:
             raise ValueError(
                 "Could not find project version in `pyproject.toml`"
             )
+
+
+def compute_solar_noon_time(
+    lat: float,
+    lon: float,
+    date: datetime.date,
+) -> datetime.datetime:
+    start_time = datetime.datetime.combine(
+        date, datetime.time.min, tzinfo=datetime.UTC
+    )
+    end_time = start_time + datetime.timedelta(days=1)
+    timescale = skyfield.api.load.timescale()
+    ephemeris: Any = skyfield.iokit.Loader(
+        tum_esm_utils.files.rel_to_abs_path("../../data")
+    )('de421.bsp')
+
+    times, events = skyfield.almanac.find_discrete(
+        timescale.from_datetime(start_time),
+        timescale.from_datetime(end_time),
+        skyfield.almanac.meridian_transits(
+            ephemeris, ephemeris['Sun'],
+            skyfield.api.wgs84.latlon(
+                latitude_degrees=lat,
+                longitude_degrees=lon,
+            )
+        ),
+    )
+
+    # Select transits instead of antitransits.
+    t = times[events == 1][0].astimezone(datetime.UTC)
+    assert isinstance(t, datetime.datetime)
+    return t
