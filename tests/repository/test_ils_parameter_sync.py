39237e2954476ed2ccda2b7fb61d3e68b80a7e74
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-01-31 20:53:51 +0100

    Expand ILS sync test to all retrieval algorithms

--- tests/repository/test_ils_parameter_sync.py
+++ tests/repository/test_ils_parameter_sync.py
@@ -10,11 +10,18 @@ def test_ils_parameter_sync() -> None:
     r = requests.get(url)
     assert r.status_code == 200
     remote_file_content = r.text.strip("\n\t ")
+    remote_ILS_lines = set(remote_file_content.split("\n"))
 
-    with open(
-        tum_esm_utils.files.
-        rel_to_abs_path("../../src/retrieval/utils/ils-parameters.csv")
-    ) as f:
-        local_file_content = f.read().strip("\n\t ")
+    for local_path in [
+        "../../src/retrieval/utils/ils-parameters.csv",
+        "../../src/retrieval/algorithms/proffast-2.2/main/prfpylot/ILSList.csv",
+        "../../src/retrieval/algorithms/proffast-2.2/main/prfpylot/ILSList.csv"
+    ]:
+        with open(tum_esm_utils.files.rel_to_abs_path(local_path)) as f:
+            local_file_content = f.read().strip("\n\t ")
 
-    assert remote_file_content == local_file_content.strip("\n\t ")
+        local_ILS_lines = set(local_file_content.split("\n"))
+        lines_missing_locally = remote_ILS_lines.difference(local_ILS_lines)
+        assert len(
+            lines_missing_locally
+        ) == 0, f"Missing lines in {local_path}: {lines_missing_locally}"
