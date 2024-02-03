3638276ae4d56c16def0be1a838e7560545db11e
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-02-03 16:38:13 +0100

    Update README, clean up docs folder

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,13 +1,13 @@
-const withNextra = require('nextra')({
-    theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx',
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
 });
 
 module.exports = withNextra({
-    images: {
-        unoptimized: true,
-    },
-    output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
 });
 
 // If you have other Next.js configurations, you can pass them as the parameter:
--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -2,8 +2,8 @@
 // https://tailwindcss.com/docs/using-with-preprocessors
 /** @type {import('postcss').Postcss} */
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
