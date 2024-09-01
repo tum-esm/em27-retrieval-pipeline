bab2b490c1c418318f1dd5720daa2f294a3127f5
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-09-01 16:39:02 +0000

    Refactor profile queries (2)
    Refactor requesting logic. It was working fine before but it was not clear where the active requests were filtered from the desired data.

--- src/profiles/main.py
+++ src/profiles/main.py
@@ -37,8 +37,8 @@ def run() -> None:
             print("No scope defined, skipping on-demand data download")
             return
 
-        for version in config.profiles.scope.models:
-            print(f"Downloading on-demand {version} data")
+        for profile_model in config.profiles.scope.models:
+            print(f"Downloading on-demand {profile_model} data")
 
             with ftplib.FTP(
                 host="ccycle.gps.caltech.edu",
@@ -48,22 +48,25 @@ def run() -> None:
             ) as ftp:
                 print("Connected to FTP server")
                 cache = profiles.cache.DownloadQueryCache.load()
-                running_queries = cache.get_active_queries(version)
+                running_queries = cache.get_active_queries(profile_model)
                 print(f"Found {len(running_queries)} already requested queries")
                 still_running_query_count = len(running_queries)
                 if len(running_queries) > 0:
                     print(f"Trying to download {len(running_queries)} queries")
                     fulfilled_queries = profiles.download_logic.download_data(
-                        config, running_queries, ftp, version
+                        config, running_queries, ftp, profile_model
                     )
                     print(
                         f"Successfully downloaded {len(fulfilled_queries)} queries"
                     )
-                    cache.remove_queries(version, fulfilled_queries)
+                    cache.remove_queries(profile_model, fulfilled_queries)
+
                     cache.dump()
                     print("Updated cache")
-                    still_running_query_count = len(running_queries
-                                                   ) - len(fulfilled_queries)
+
+                    still_running_query_count = len(
+                        cache.get_timed_out_queries(profile_model)
+                    )
 
                 open_query_count = config.profiles.server.max_parallel_requests - still_running_query_count
 
@@ -79,33 +82,49 @@ def run() -> None:
                     print(f"{open_query_count} open slots for new queries")
 
                 # Generate daily sensor sets
-                missing_queries = profiles.generate_queries.generate_download_queries(
-                    config, version
+                outstanding_download_queries = profiles.generate_queries.generate_download_queries(
+                    config, profile_model
                 )
-                if len(missing_queries) == 0:
+
+                if len(outstanding_download_queries) == 0:
                     print("No data to request.")
                     continue
 
                 # queries might not be in cache anymore but still
                 # downloadable from the server
-                print(f"Trying to download {len(missing_queries)} queries")
-                fulfilled_queries = profiles.download_logic.download_data(
-                    config, missing_queries, ftp, version
+                print(
+                    f"Trying to download {len(outstanding_download_queries)} queries"
                 )
-                missing_queries = sorted(
-                    set(missing_queries).difference(set(fulfilled_queries)),
+                fulfilled_queries = profiles.download_logic.download_data(
+                    config, outstanding_download_queries, ftp, profile_model
+                )
+                outstanding_download_queries = sorted(
+                    set(outstanding_download_queries).difference(
+                        set(fulfilled_queries)
+                    ),
                     key=lambda q: q.from_date,
                     reverse=True,
                 )
                 print(
                     f"Successfully downloaded {len(fulfilled_queries)} queries"
                 )
-                query_count = min(open_query_count, len(missing_queries))
+                new_download_queries = sorted(
+                    list(
+                        set(outstanding_download_queries) -
+                        set(fulfilled_queries) -
+                        set(cache.get_active_queries(profile_model))
+                    ),
+                    key=lambda q: q.from_date,
+                    reverse=True,
+                )
+
+                query_count = min(open_query_count, len(new_download_queries))
                 print(
-                    f"Requesting {query_count} out of {len(missing_queries)} queries"
+                    f"Requesting {query_count} out of {len(new_download_queries)} queries"
                 )
                 profiles.upload_logic.upload_requests(
-                    config, missing_queries[: query_count], ftp, version
+                    config, new_download_queries[: query_count], ftp,
+                    profile_model
                 )
                 print(
                     "Done. Run this script again (after waiting " +
