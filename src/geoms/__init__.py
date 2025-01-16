4fc277733ecf709aa502bba27af45b0a3770c2d3
Moritz Makowski <moritz.makowski@tum.de>
2025-01-16 10:33:59 +0000

    GEOMS documentation (1)
    add correct copyright information

--- src/geoms/__init__.py
+++ src/geoms/__init__.py
@@ -1,1 +1,8 @@
+"""Write data into GEOMS compliant HDF5 files.
+
+This code has been adapted from the PROFFASTpylot (https://doi.org/10.21105/joss.06481)
+which is licensed under the GNU General Public License version 3. The authors of the
+original code are Lena Feld, Benedikt Herkommer, Darko Dubravica affiliated with the
+Karlsruhe Institut of Technology (KIT)."""
+
 from . import main as main
--- src/geoms/main.py
+++ src/geoms/main.py
@@ -1,3 +1,10 @@
+"""Write data into GEOMS compliant HDF5 files.
+
+This code has been adapted from the PROFFASTpylot (https://doi.org/10.21105/joss.06481)
+which is licensed under the GNU General Public License version 3. The authors of the
+original code are Lena Feld, Benedikt Herkommer, Darko Dubravica affiliated with the
+Karlsruhe Institut of Technology (KIT)."""
+
 from typing import Literal, Optional
 import datetime
 import os
@@ -288,7 +295,7 @@ def run() -> None:
                     sorted(results_within_time_range), dynamic_ncols=True, desc="..."
                 )
                 for result in progress:
-                    progress.desc = f"{result}"
+                    progress.desc = f"{sensor_id}/{result}"
                     filepath, status_message = generate_geoms_file(
                         os.path.join(results_folders, result),
                         geoms_metadata,
