23b8be330ee0b16f1611cd0d4e7ff81b4bf2df47
Moritz Makowski <moritz.makowski@tum.de>
2025-07-09 21:59:30 +0000

    #127 Store atmospheric profiles in subdirectories for year/month (3)
    update retrieval

--- src/retrieval/session/move_profiles.py
+++ src/retrieval/session/move_profiles.py
@@ -32,17 +32,28 @@ def run(
         ]
     try:
         for src, dst in files_to_copy:
-            shutil.copy(
-                os.path.join(
-                    config.general.data.atmospheric_profiles.root,
-                    session.atmospheric_profile_model,
-                    src,
-                ),
-                os.path.join(
-                    session.ctn.data_input_path,
-                    "map",
-                    dst,
-                ),
+            src_path_1 = os.path.join(
+                config.general.data.atmospheric_profiles.root,
+                session.atmospheric_profile_model,
+                src,
             )
+            src_path_2 = os.path.join(
+                config.general.data.atmospheric_profiles.root,
+                session.atmospheric_profile_model,
+                src[:4],
+                src[4:6],
+                src,
+            )
+            dst_path = os.path.join(
+                session.ctn.data_input_path,
+                "map",
+                dst,
+            )
+            if os.path.isfile(src_path_1):
+                shutil.copy(src_path_1, dst_path)
+            elif os.path.isfile(src_path_2):
+                shutil.copy(src_path_2, dst_path)
+            else:
+                raise FileNotFoundError(f"File not found: {src_path_1} or {src_path_2}")
     except FileNotFoundError as e:
         raise AssertionError(f"map file does not exist: {e}")
