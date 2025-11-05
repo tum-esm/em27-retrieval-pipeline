89ca460263c6b8466ee6684e44fd96ecc1b4aabf
Moritz Makowski <moritz.makowski@tum.de>
2025-11-05 16:36:04 +0000

    Ignore GEOMS code in mypy check

--- src/geoms/geoms_api.py
+++ src/geoms/geoms_api.py
@@ -1,11 +1,12 @@
+# pyright: basic
+# type: ignore
+
 """Write data into GEOMS compliant HDF5 files.
 
 This code has been adapted from the PROFFASTpylot (https://doi.org/10.21105/joss.06481)
 which is licensed under the GNU General Public License version 3. The authors of the
 original code are Lena Feld, Benedikt Herkommer, Darko Dubravica affiliated with the
 Karlsruhe Institut of Technology (KIT)."""
-
-# pyright: basic
 
 from typing import Any, Callable, Literal
 import math
