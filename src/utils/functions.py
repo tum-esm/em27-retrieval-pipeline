51fd4d7ef78004f20423d37d4bde0fd49885a80e
Moritz Makowski <moritz.makowski@tum.de>
2025-11-05 14:37:05 +0000

    #138 Add retrieval diagnostics to bundle outputs (2)
    Add function to load `.dat` files

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,5 +1,7 @@
 import datetime
-
+import os
+from typing import Optional
+import polars as pl
 import em27_metadata
 import tum_esm_utils
 
@@ -29,3 +31,47 @@ def get_pipeline_version() -> str:
             return v
         except (KeyError, AssertionError):
             raise ValueError("Could not find project version in `pyproject.toml`")
+
+
+def load_dat_file(
+    filepath: str,
+    possible_separators: list[str] = ["!", "?", "&"],
+) -> pl.DataFrame:
+    """Loads a `.dat` file as a Polars DataFrame.
+
+    The separator should be a character that does not occur in the file itself."""
+
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"File not found: {filepath}")
+
+    filecontent = tum_esm_utils.files.load_file(filepath)
+    separator: Optional[str] = None
+    for s in possible_separators:
+        if s not in filecontent:
+            separator = s
+            break
+    if separator is None:
+        raise ValueError(f"All possible separators occur in the file: {possible_separators}")
+
+    columns = [
+        c
+        for c in filecontent.split("\n")[0].replace(",", " ").replace("\t", " ").split(" ")
+        if c != ""
+    ]
+    return (
+        pl.read_csv(filepath, new_columns=["full"], separator=separator)
+        .with_columns(
+            pl.col("full")
+            .str.replace_all(",", " ")
+            .str.split(" ")
+            .list.filter(pl.element().str.len_chars().gt(0))
+            .alias("splitted")
+        )
+        .with_columns(
+            [
+                pl.col("splitted").list.get(i).str.strip_chars(" ").alias(columns[i])
+                for i in range(len(columns))
+            ]
+        )
+        .drop("full", "splitted")
+    )
