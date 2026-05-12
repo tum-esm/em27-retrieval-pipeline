fc4079a53b32d423f1d8d387b9a4ebf686788ecc
Moritz Oliveira Makowski <moritz.makowski@tum.de>
2026-05-12 00:47:09 +0000

    Enable example tests in CI

--- tests/repository/test_full_example.py
+++ tests/repository/test_full_example.py
@@ -6,7 +6,7 @@ PROJECT_DIR = tum_esm_utils.files.rel_to_abs_path("../../")
 
 
 @pytest.mark.order(4)
-@pytest.mark.complete
+@pytest.mark.ci
 def test_full_example() -> None:
     exit_code = os.system(f"cd {PROJECT_DIR}/example && bash run.sh")  # pyright: ignore[reportDeprecated]
     assert exit_code == 0, f"Full example failed with exit code {exit_code}"
