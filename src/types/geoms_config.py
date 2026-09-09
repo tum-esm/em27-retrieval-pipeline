49f95c3d1fdd3f2df9d532c40da9b3af29a10ae0
Moritz Oliveira Makowski <moritz.makowski@tum.de>
2026-09-09 13:49:20 +0000

    #154 Migrate configuration from JSON to TOML (7)
    Add new GEOMS config schema

---
+++ src/types/geoms_config.py
@@ -0,0 +1,200 @@
+from __future__ import annotations
+
+import datetime
+import os
+from typing import Optional
+
+import dotenv
+import pydantic
+import tum_esm_utils
+
+
+class GEOMSMetadataConfigs:
+    class General(pydantic.BaseModel):
+        network: str = pydantic.Field(
+            ...,
+            description="Used in the filename of the HDF5 file",
+            examples=["FTIR.COCCON"],
+        )
+        affiliation: str = pydantic.Field(
+            ...,
+            description="Used in the filename of the HDF5 file",
+            examples=["TUM.ESM"],
+        )
+        pressure_sensor_name: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `SURFACE.PRESSURE_INDEPENDENT_SOURCE`",
+            examples=["young-61302", "vaisala-PTB330"],
+        )
+
+    class Data(pydantic.BaseModel):
+        discipline: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `DATA_DISCIPLINE`",
+            examples=["ATMOSPHERIC.CHEMISTRY;REMOTE.SENSING;GROUNDBASED"],
+        )
+        group: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `DATA_GROUP`",
+            examples=["EXPERIMENTAL;PROFILE.STATIONARY"],
+        )
+        file_version: int = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `DATA_FILE_VERSION`",
+            examples=[1],
+        )
+        quality: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `DATA_QUALITY`",
+            examples=["Station data."],
+        )
+        template: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `DATA_TEMPLATE`",
+            examples=["GEOMS-TE-FTIR-COCCON-001"],
+        )
+
+    class File(pydantic.BaseModel):
+        doi: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `FILE_DOI`",
+        )
+        meta_version: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `FILE_META_VERSION`",
+            examples=["04R088;CUSTOM"],
+        )
+        access: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `FILE_ACCESS`",
+            examples=["COCCON"],
+        )
+        project_id: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `FILE_PROJECT_ID`",
+            examples=["COCCON"],
+        )
+
+    class Contact(pydantic.BaseModel):
+        name: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `PI_NAME`/`DO_NAME`/`DS_NAME`",
+            examples=["Makowski;Moritz"],
+        )
+        email: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `PI_EMAIL`/`DO_EMAIL`/`DS_EMAIL`",
+            examples=["moritz.makowski@tum.de"],
+        )
+        affiliation: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `PI_AFFILIATION`/`DO_AFFILIATION`/`DS_AFFILIATION`",
+            examples=[
+                "Technical University of Munich - Professorship of Environmental Sensing and Modeling;TUM.ESM"
+            ],
+        )
+        address: str = pydantic.Field(
+            ...,
+            description="The value of the HDF5 attribute `PI_ADDRESS`/`DO_ADDRESS`/`DS_ADDRESS`",
+            examples=["Theresienstr. 90;D-80333 Munich;GERMANY"],
+        )
+
+
+class GEOMSCalibrationFactorsConfigs:
+    class CalibrationFactors(pydantic.BaseModel):
+        model_config = pydantic.ConfigDict(arbitrary_types_allowed=True)
+
+        sensor_id: str
+        valid_from_datetime: datetime.datetime = pydantic.Field(...)
+        valid_to_datetime: datetime.datetime = pydantic.Field(...)
+        xco2: float = pydantic.Field(
+            ..., description="Calibration factor for carbon dioxide: xco2_cal = xco2_raw * factor"
+        )
+        xch4: float = pydantic.Field(
+            ..., description="Calibration factor for methane: xch4_cal = xch4_raw * factor"
+        )
+        xco: float = pydantic.Field(
+            ..., description="Calibration factor for carbon monoxide: xco_cal = xco_raw * factor"
+        )
+        xh2o: float = pydantic.Field(
+            ..., description="Calibration factor for water vapor: xh2o_cal = xh2o_raw * factor"
+        )
+
+        @staticmethod
+        @pydantic.field_validator("root", mode="after")
+        def validate_times(
+            v: GEOMSCalibrationFactorsConfigs.CalibrationFactors,
+        ) -> GEOMSCalibrationFactorsConfigs.CalibrationFactors:
+            if v.valid_from_datetime >= v.valid_to_datetime:
+                raise ValueError(
+                    f"valid_from_datetime {v.valid_from_datetime} should be less than valid_to_datetime {v.valid_to_datetime}"
+                )
+            return v
+
+    class CalibrationFactorsList(pydantic.RootModel[list[CalibrationFactors]]):
+        root: list[GEOMSCalibrationFactorsConfigs.CalibrationFactors]
+
+        @staticmethod
+        @pydantic.field_validator("root", mode="after")
+        def validate_sensor_ids(
+            vs: GEOMSCalibrationFactorsConfigs.CalibrationFactorsList,
+        ) -> GEOMSCalibrationFactorsConfigs.CalibrationFactorsList:
+            sensor_ids = set([v.sensor_id for v in vs.root])
+            for sensor_id in sensor_ids:
+                sensor_values = sorted(
+                    [v for v in vs.root if v.sensor_id == sensor_id],
+                    key=lambda x: x.valid_from_datetime,
+                )
+                for v1, v2 in zip(sensor_values[:-1], sensor_values[1:]):
+                    if v1.valid_to_datetime > v2.valid_from_datetime:
+                        raise ValueError(
+                            f"Overlapping calibration factors for sensor {sensor_id}: {v1.valid_to_datetime} > {v2.valid_from_datetime}"
+                        )
+            return vs
+
+        def get_index(self, sensor_id: str, datetime: datetime.datetime) -> Optional[int]:
+            """Get the calibration factors for the specified sensor."""
+            try:
+                return next(
+                    i
+                    for i, v in enumerate(self.root)
+                    if v.sensor_id == sensor_id
+                    and v.valid_from_datetime <= datetime
+                    and v.valid_to_datetime >= datetime
+                )
+            except StopIteration:
+                return None
+
+
+class GEOMSMetadataConfig(pydantic.BaseModel):
+    general: GEOMSMetadataConfigs.General
+    data: GEOMSMetadataConfigs.Data
+    file: GEOMSMetadataConfigs.File
+    principle_investigator: GEOMSMetadataConfigs.Contact
+    data_originator: GEOMSMetadataConfigs.Contact
+    data_submitter: GEOMSMetadataConfigs.Contact
+    locations: dict[str, str] = pydantic.Field(
+        ..., description="Maps your locations id to the corresponding EVDC location id"
+    )
+
+
+class GEOMSConfig(pydantic.BaseModel):
+    metadata: GEOMSMetadataConfig
+    calibration_factors: GEOMSCalibrationFactorsConfigs.CalibrationFactorsList
+
+    # TODO: support loading old format
+
+    @staticmethod
+    def load(template: bool = False) -> GEOMSConfig:
+        """Load the EVDC metadata from `<config_dir>/geoms_metadata.json`."""
+
+        erp_config_dir = tum_esm_utils.files.rel_to_abs_path("../../config")
+        if not template:
+            env_path = os.path.join(tum_esm_utils.files.rel_to_abs_path("../../config"), ".env")
+            if os.path.isfile(env_path):
+                dotenv.load_dotenv(env_path)
+            erp_config_dir = os.getenv("ERP_CONFIG_DIR", erp_config_dir)
+        filepath = os.path.join(
+            erp_config_dir, f"geoms_config{'.template' if template else ''}.toml"
+        )
+        return GEOMSConfig.model_validate_json(tum_esm_utils.files.load_file(filepath))
